fda2809ef882553e185d1c425b5f88d6ab5cfd55
Chris <100044659+ChrisWangCA@users.noreply.github.com>
2022-09-02 13:11:34 -0600

    Time: 0 ms (100.00%), Space: 41.8 MB (98.71%) - LeetHub

--- 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.java
+++ 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.java
@@ -20,6 +20,6 @@ class Solution {
         }
         int left = maxDepth(root.left);
         int right = maxDepth(root.right);
-        return 1 + Math.max(left,right);
+        return Math.max(left,right) + 1;
     }
 }
