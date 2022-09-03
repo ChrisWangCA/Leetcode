9504eae8780e0c520947f2d4b62f90494754fe94
Chris <100044659+ChrisWangCA@users.noreply.github.com>
2022-09-03 13:42:30 -0600

    Update 110-balanced-binary-tree.java

--- 110-balanced-binary-tree/110-balanced-binary-tree.java
+++ 110-balanced-binary-tree/110-balanced-binary-tree.java
@@ -16,7 +16,7 @@
 class Solution {
     public boolean isBalanced(TreeNode root) {
         if(root == null) return true;
-        return Math.abs(balanced(root.left) - balanced(root.right)) <= 1 &&                 isBalanced(root.left) && isBalanced(root.right);
+        return Math.abs(balanced(root.left) - balanced(root.right)) <= 1 && isBalanced(root.left) && isBalanced(root.right);
     }
     public int balanced(TreeNode root){
         if(root == null) return 0;
