8a4a5e1a69a39019513e6821130a63f2c03ba6bf
Chris <100044659+ChrisWangCA@users.noreply.github.com>
2022-09-03 22:59:45 -0600

    Time: 1 ms (27.03%), Space: 42 MB (48.88%) - LeetHub

--- 404-sum-of-left-leaves/404-sum-of-left-leaves.java
+++ 404-sum-of-left-leaves/404-sum-of-left-leaves.java
@@ -16,19 +16,19 @@
 class Solution {
     public int sumOfLeftLeaves(TreeNode root) {
         int sum = 0;
-        if(root == null) return 0;
+        if(root==null) return sum;
         Queue<TreeNode> queue = new LinkedList<>();
         queue.offer(root);
         while(!queue.isEmpty()){
-                TreeNode temp = queue.poll();
-                if(temp.left != null){
-                    queue.offer(temp.left);
-                    if(temp.left.left == null && temp.left.right == null){
-                        sum += temp.left.val;
-                    }
+            TreeNode temp = queue.poll();
+            if(temp.left!=null){
+                queue.offer(temp.left);
+                if(temp.left.left == null && temp.left.right == null){
+                    sum += temp.left.val;
                 }
-                if(temp.right != null){
-                    queue.offer(temp.right);
+            }
+            if(temp.right!=null){
+                queue.offer(temp.right);
             }
         }
         return sum;
