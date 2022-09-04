db977bd6a3c221aad07b081f7308106068d7e742
Chris <100044659+ChrisWangCA@users.noreply.github.com>
2022-09-03 23:16:31 -0600

    Time: 1 ms (100.00%), Space: 44.4 MB (68.01%) - LeetHub

--- 113-path-sum-ii/113-path-sum-ii.java
+++ 113-path-sum-ii/113-path-sum-ii.java
@@ -16,26 +16,26 @@
 class Solution {
     public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
         List<List<Integer>> res = new ArrayList<>();
-        List<Integer> path = new ArrayList<>();
+        List<Integer> list = new ArrayList<>();
         if(root == null) return res;
-        traversal(root,targetSum,res,path);
+        recursion(root,targetSum,res,list);
         return res;
     }
-    private void traversal(TreeNode root,int target,List<List<Integer>> res,List<Integer> path){
-        path.add(root.val);
+    private void recursion(TreeNode root,int target,List<List<Integer>> res,List<Integer> list){
+        list.add(root.val);
         if(root.left == null && root.right == null){
             if(target - root.val == 0){
-                res.add(new ArrayList<>(path));
+                res.add(new ArrayList<>(list));
             }
             return;
         }
         if(root.left != null){
-            traversal(root.left,target - root.val,res,path);
-            path.remove(path.size()-1);
+            recursion(root.left,target - root.val,res,list);
+            list.remove(list.size()-1);
         }
         if(root.right != null){
-            traversal(root.right,target - root.val,res,path);
-            path.remove(path.size()-1);
+            recursion(root.right,target - root.val,res,list);
+            list.remove(list.size()-1);
         }
     }
 }
