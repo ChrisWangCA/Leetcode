f5fc64a17fff5f20d26a7884732497061d05e1bc
Chris <100044659+ChrisWangCA@users.noreply.github.com>
2022-09-04 11:43:42 -0600

    Time: 0 ms (100.00%), Space: 42 MB (61.82%) - LeetHub

---
+++ 59-spiral-matrix-ii/59-spiral-matrix-ii.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int[][] generateMatrix(int n) {
+        int[][] res = new int[n][n];
+        int start = 0;
+        int count = 1;
+        int loop = 0;
+        int i,j;
+        while(loop ++ < n/2){
+            for(j=start;j<n-loop;j++){
+                res[start][j] = count++;
+            }
+            for(i=start;i<n-loop;i++){
+                res[i][j] = count++;
+            }
+            for(;j>=loop;j--){
+                res[i][j] = count++;
+            }
+            for(;i>=loop;i--){
+                res[i][j] = count++;
+            }
+            start++;
+        }
+        if(n % 2 == 1){
+            res[start][start] = count++;
+        }
+        return res;
+    }
+}
