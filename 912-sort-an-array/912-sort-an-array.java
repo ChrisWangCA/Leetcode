99dfe556a094dc0f404567d427b143f0a034ac06
Chris <100044659+ChrisWangCA@users.noreply.github.com>
2022-09-13 13:41:31 -0600

    Time: 57 ms (23.14%), Space: 76.9 MB (15.56%) - LeetHub

--- 912-sort-an-array/912-sort-an-array.java
+++ 912-sort-an-array/912-sort-an-array.java
@@ -1,39 +1,39 @@
 class Solution {
     public int[] sortArray(int[] nums) {
-        int[] arr = new int[nums.length];
-        if(nums == null || nums.length < 2) return arr;
+        int[] res = new int[nums.length];
+        if(nums == null || nums.length < 2) return res;
         quickSort(nums,0,nums.length-1);
-        for(int i=0;i<arr.length;i++){
-            arr[i] = nums[i];
+        for(int i=0;i<res.length;i++){
+            res[i] = nums[i];
         }
-        return arr;
-    }
-    public static void quickSort(int[] nums,int L,int R){
-        if(L<R){
-            swap(nums,L+(int)(Math.random()*(R-L+1)),R);
-            int[] p = partition(nums,L,R);
-            quickSort(nums,L,p[0]-1);
-            quickSort(nums,p[1]+1,R);
+        return res;
+    };
+    public void quickSort(int[] nums,int l,int r){
+        if(l<r){
+            swap(nums,l+(int)(Math.random()*(r-l+1)),r);
+            int[] p = partition(nums,l,r);
+            quickSort(nums,l,p[0]-1);
+            quickSort(nums,p[1]+1,r);
         }
     }
-    public static int[] partition(int[] nums,int L,int R){
-        int less = L - 1;
-        int more = R;
-        while(L<more){
-            if(nums[L] < nums[R]){
-                swap(nums,++less,L++);
-            }else if(nums[L] > nums[R]){
-                swap(nums,--more,L);
+    public int[] partition(int[] arr,int l,int r){
+        int less = l-1;
+        int more = r;
+        while(l<more){
+            if(arr[l] < arr[r]){
+                swap(arr,++less,l++);
+            }else if(arr[l] > arr[r]){
+                swap(arr,--more,l);
             }else{
-                L++;
+                l++;
             }
         }
-        swap(nums,more,R);
+        swap(arr,more,r);
         return new int[]{less+1,more};
     }
-    public static void swap(int[] nums,int i,int j){
-        int temp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = temp;
+    public void swap(int[] arr,int i,int j){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     }
 }
