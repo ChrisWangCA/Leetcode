08cbaf3287efe4d5da94b9f92d9de456f93e82d9
Chris <100044659+ChrisWangCA@users.noreply.github.com>
2022-09-14 15:25:23 -0600

    Time: 18 ms (80.55%), Space: 51.8 MB (48.47%) - LeetHub

--- 459-repeated-substring-pattern/459-repeated-substring-pattern.java
+++ 459-repeated-substring-pattern/459-repeated-substring-pattern.java
@@ -1,22 +1,18 @@
 class Solution {
     public boolean repeatedSubstringPattern(String s) {
-        if(s.equals("")){
-            return false;
-        }
+        if(s.equals("")) return false;
         int size = s.length();
         s = " " + s;
-        char[] ch = s.toCharArray();
+        char[] chars = s.toCharArray();
         int[] next = new int[size+1];
         for(int i=2,j=0;i<=size;i++){
-            while(j > 0 && ch[i]!=ch[j+1]){
+            while(j>0 && chars[i]!=chars[j+1]){
                 j = next[j];
             }
-            if(ch[i] == ch[j+1]){
-                j++;
-            }
+            if(chars[i] == chars[j+1]) j++;
             next[i] = j;
         }
-        if(next[size] > 0 && size % (size - next[size]) == 0){
+        if(next[size]>0 && size%(size-next[size])==0){
             return true;
         }
         return false;
