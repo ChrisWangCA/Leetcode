58297b9b1942eedfd5c74d015d5634c6a73a07c7
Chris Wang <100044659+ChrisWangCA@users.noreply.github.com>
2022-09-16 16:37:16 -0600

    Time: 103 ms (19.32%), Space: 154.9 MB (23.84%) - LeetHub

--- 239-sliding-window-maximum/239-sliding-window-maximum.java
+++ 239-sliding-window-maximum/239-sliding-window-maximum.java
@@ -1,13 +1,13 @@
 class Solution {
-    Deque<Integer> queue = new LinkedList<>();
+    Deque<Integer> queue = new ArrayDeque<>();
     class MyQueue{
         void pop(int val){
             if(!queue.isEmpty() && val == queue.peek()){
                 queue.poll();
             }
         }
-        void push(int val){
-            while(!queue.isEmpty() && val > queue.getLast()){
+        void add(int val){
+            while(!queue.isEmpty() && val > queue.peekLast()){
                 queue.removeLast();
             }
             queue.add(val);
@@ -19,16 +19,16 @@ class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
         if(nums.length == 1) return nums;
         int len = nums.length - k + 1;
-        int[] res = new int[len];
+        int[] res = new int[len];        
         int index = 0;
         MyQueue queue = new MyQueue();
         for(int i=0;i<k;i++){
-            queue.push(nums[i]);
+            queue.add(nums[i]);
         }
         res[index++] = queue.max();
         for(int i=k;i<nums.length;i++){
-            queue.pop(nums[i - k]);
-            queue.push(nums[i]);
+            queue.pop(nums[i-k]);
+            queue.add(nums[i]);
             res[index++] = queue.max();
         }
         return res;
