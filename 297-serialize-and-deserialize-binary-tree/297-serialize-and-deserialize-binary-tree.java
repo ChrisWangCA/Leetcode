15ff8911798718221faf03869fd7dfd843bf3f8a
Chris Wang <100044659+ChrisWangCA@users.noreply.github.com>
2022-09-19 17:53:12 -0600

    Time: 32 ms (41.59%), Space: 55 MB (21.22%) - LeetHub

--- 297-serialize-and-deserialize-binary-tree/297-serialize-and-deserialize-binary-tree.java
+++ 297-serialize-and-deserialize-binary-tree/297-serialize-and-deserialize-binary-tree.java
@@ -20,18 +20,16 @@ public class Codec {
 
     // Decodes your encoded data to tree.
     public TreeNode deserialize(String data) {
-        String[] vals = data.split("_");
+        String[] arr = data.split("_");
         Queue<String> queue = new LinkedList<>();
-        for(int i=0;i<vals.length;i++){
-            queue.add(vals[i]);
-        }
+        for(int i=0;i<arr.length;i++) queue.offer(arr[i]);
         return deserialize(queue);
     }
     
     public TreeNode deserialize(Queue<String> queue){
-        String val = queue.poll();
-        if(val.equals("#")) return null;
-        TreeNode root = new TreeNode(Integer.valueOf(val));
+        String res = queue.poll();
+        if(res.equals("#")) return null;
+        TreeNode root = new TreeNode(Integer.valueOf(res));
         root.left = deserialize(queue);
         root.right = deserialize(queue);
         return root;
