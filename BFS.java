35310b0e82434ef8a56749c947ea63707dad2010
Chris Wang <100044659+ChrisWangCA@users.noreply.github.com>
2022-09-20 21:26:58 -0600

    Add files via upload

---
+++ BFS.java
@@ -0,0 +1,29 @@
+package src.graph;
+
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class BFS {
+
+	public static void bfs(Node node) {
+		if (node == null) {
+			return;
+		}
+		Queue<Node> queue = new LinkedList<>();
+		HashSet<Node> map = new HashSet<>();
+		queue.add(node);
+		map.add(node);
+		while (!queue.isEmpty()) {
+			Node cur = queue.poll();
+			System.out.println(cur.value);
+			for (Node next : cur.nexts) {
+				if (!map.contains(next)) {
+					map.add(next);
+					queue.add(next);
+				}
+			}
+		}
+	}
+
+}
