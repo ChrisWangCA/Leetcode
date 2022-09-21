0cef95158182b4c429fff8c4840929b5feef711b
Chris Wang <100044659+ChrisWangCA@users.noreply.github.com>
2022-09-20 22:27:16 -0600

    Time: 88 ms (29.10%), Space: 70 MB (20.67%) - LeetHub

---
+++ 208-implement-trie-prefix-tree/208-implement-trie-prefix-tree.java
@@ -0,0 +1,67 @@
+class Trie {
+    public class TrieNode{
+        int pass;
+        int end;
+        TrieNode[] nexts;
+        TrieNode(){
+            pass = 0;
+            end = 0;
+            nexts = new TrieNode[26];
+        }
+    }
+    
+    private TrieNode root;
+    public Trie() {
+        root = new TrieNode();
+    }
+    
+    public void insert(String word) {
+        if(word == null) return;
+        char[] chs = word.toCharArray();
+        TrieNode node = root;
+        int index = 0;
+        for(int i=0;i<chs.length;i++){
+            index = chs[i] - 'a';
+            if(node.nexts[index] == null){
+                node.nexts[index] = new TrieNode();
+            }
+            node = node.nexts[index];
+            node.pass++;
+        }
+        node.end++;
+    }
+    
+    public boolean search(String word) {
+        if(word == null) return false;
+        char[] chs = word.toCharArray();
+        TrieNode node = root;
+        int index = 0;
+        for(int i=0;i<chs.length;i++){
+            index = chs[i] - 'a';
+            if(node.nexts[index] == null) return false;
+            node = node.nexts[index];
+        }
+        return node.end != 0;
+    }
+    
+    public boolean startsWith(String prefix) {
+        if(prefix == null) return false;
+        char[] chs = prefix.toCharArray();
+        TrieNode node = root;
+        int index = 0;
+        for(int i=0;i<chs.length;i++){
+            index = chs[i] - 'a';
+            if(node.nexts[index] == null) return false;
+            node = node.nexts[index];
+        }
+        return true;
+    }
+}
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie obj = new Trie();
+ * obj.insert(word);
+ * boolean param_2 = obj.search(word);
+ * boolean param_3 = obj.startsWith(prefix);
+ */
