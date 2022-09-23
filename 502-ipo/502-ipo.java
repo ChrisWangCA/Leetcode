2bbb445312cca420609ea06dd436e3bc76ef0846
Chris Wang <100044659+ChrisWangCA@users.noreply.github.com>
2022-09-22 20:09:00 -0600

    Time: 253 ms (20.51%), Space: 105.7 MB (19.42%) - LeetHub

--- 502-ipo/502-ipo.java
+++ 502-ipo/502-ipo.java
@@ -1,30 +1,30 @@
 class Solution {
-	public static class Node{
-		int p;
-		int c;
-		public Node(int p,int c){
-			this.p = p;
-			this.c = c;
-		}
-	}
-
-	public int findMaximizedCapital(int k, int w, int[] profits, int[] capital) {
-		Node[] nodes = new Node[profits.length];
-		for(int i = 0;i < profits.length; i++){
-			nodes[i] = new Node(profits[i],capital[i]);
-		}
-		PriorityQueue<Node> min = new PriorityQueue<>((o1,o2) -> o1.c - o2.c);
-		PriorityQueue<Node> max = new PriorityQueue<>((o1,o2) -> o2.p - o1.p);
-		for(int i=0;i<nodes.length;i++){
-			min.add(nodes[i]);
-		}
-		for(int i=0;i<k;i++){
-			while(!min.isEmpty() && min.peek().c <= w){
-				max.add(min.poll());
-			}
-			if(max.isEmpty()) return w;
-			w += max.poll().p;
-		}
-		return w;
-	}
+    public class Node{
+        int p;
+        int c;
+        public Node(int p,int c){
+            this.p=p;
+            this.c=c;
+        }
+    }
+    
+    public int findMaximizedCapital(int k, int w, int[] profits, int[] capital) {
+        Node[] nodes = new Node[profits.length];
+        for(int i=0;i<profits.length;i++){
+            nodes[i] = new Node(profits[i],capital[i]);
+        }
+        PriorityQueue<Node> min = new PriorityQueue<>((o1,o2)->o1.c -o2.c);
+        PriorityQueue<Node> max = new PriorityQueue<>((o1,o2)->o2.p -o1.p);
+        for(int i=0;i<nodes.length;i++){
+            min.add(nodes[i]);
+        }
+        for(int i=0;i<k;i++){
+            while(!min.isEmpty() && min.peek().c <= w){
+                max.add(min.poll());
+            }
+            if(max.isEmpty()) return w;
+            w += max.poll().p;
+        }
+        return w;
+    }
 }
