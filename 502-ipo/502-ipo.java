47e72c6eaa079f6f46365dbfe29ff41e73e422cc
Chris Wang <100044659+ChrisWangCA@users.noreply.github.com>
2022-09-23 12:42:33 -0600

    Time: 282 ms (12.52%), Space: 101.3 MB (51.00%) - LeetHub

--- 502-ipo/502-ipo.java
+++ 502-ipo/502-ipo.java
@@ -1,29 +1,30 @@
 class Solution {
+    
     public class Node{
         int p;
         int c;
         public Node(int p,int c){
-            this.p=p;
-            this.c=c;
+            this.p = p;
+            this.c = c;
         }
     }
-    
+
     public int findMaximizedCapital(int k, int w, int[] profits, int[] capital) {
         Node[] nodes = new Node[profits.length];
         for(int i=0;i<profits.length;i++){
             nodes[i] = new Node(profits[i],capital[i]);
         }
-        PriorityQueue<Node> min = new PriorityQueue<>((o1,o2)->o1.c -o2.c);
-        PriorityQueue<Node> max = new PriorityQueue<>((o1,o2)->o2.p -o1.p);
+        PriorityQueue<Node> small = new PriorityQueue<>((o1,o2)->o1.c - o2.c);
+        PriorityQueue<Node> big = new PriorityQueue<>((o1,o2)->o2.p - o1.p);
         for(int i=0;i<nodes.length;i++){
-            min.add(nodes[i]);
+            small.add(nodes[i]);
         }
         for(int i=0;i<k;i++){
-            while(!min.isEmpty() && min.peek().c <= w){
-                max.add(min.poll());
+            while(!small.isEmpty() && small.peek().c <= w){
+                big.add(small.poll());
             }
-            if(max.isEmpty()) return w;
-            w += max.poll().p;
+            if(big.isEmpty()) return w;
+            w += big.poll().p;
         }
         return w;
     }
