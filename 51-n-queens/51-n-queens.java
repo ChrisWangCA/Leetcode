ab78cf40628778b966d001bc28b00ac5d02aba2d
Chris Wang <100044659+ChrisWangCA@users.noreply.github.com>
2022-09-23 15:54:13 -0600

    Time: 5 ms (71.25%), Space: 45.8 MB (41.09%) - LeetHub

--- 51-n-queens/51-n-queens.java
+++ 51-n-queens/51-n-queens.java
@@ -1,16 +1,16 @@
 class Solution {
-   public List<List<String>> solveNQueens(int n) {
-        int[] queen  = new int[n];
+    public List<List<String>> solveNQueens(int n) {
+        int[] queen = new int[n];
         Arrays.fill(queen,0);
         List<List<String>> res = new ArrayList<>();
         process(res,queen,0,n);
         return res;
     }
-
-    public static void process(List<List<String>> res,int[] queen,int cur,int n){
-        if (cur>=n){
+    
+    public void process(List<List<String>> res,int[] queen,int cur,int n){
+        if(cur >= n){
             List<String> list = new ArrayList<>();
-            for (int i = 0; i < n; i++) {
+            for(int i=0;i<n;i++){
                 char[] chs = new char[n];
                 Arrays.fill(chs,'.');
                 chs[queen[i]] = 'Q';
@@ -18,17 +18,17 @@ class Solution {
             }
             res.add(list);
         }
-        for (int j = 0; j < n; j++) {
-            if (isValid(queen, cur, j)) {
+        for(int j=0;j<n;j++){
+            if(isValid(queen,cur,j)){
                 queen[cur] = j;
-                process(res,queen,cur + 1, n);
+                process(res,queen,cur+1,n);
             }
         }
     }
-
-    public static boolean isValid(int[] queue, int cur, int j) {
-        for (int k = 0; k < cur; k++) {
-            if (j == queue[k] || Math.abs(queue[k] - j) == Math.abs(cur - k)) {
+    
+    public boolean isValid(int[] queen,int cur, int j){
+        for(int k=0;k<cur;k++){
+            if(j == queen[k] || Math.abs(queen[k]-j) == Math.abs(cur - k)){
                 return false;
             }
         }
