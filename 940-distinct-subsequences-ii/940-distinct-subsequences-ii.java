0cc2ab395f87405d6b345ec5b24726ab44f79119
Chris Wang <100044659+ChrisWangCA@users.noreply.github.com>
2022-09-25 14:07:22 -0600

    Time: 4 ms (95.21%), Space: 42 MB (87.43%) - LeetHub

--- 940-distinct-subsequences-ii/940-distinct-subsequences-ii.java
+++ 940-distinct-subsequences-ii/940-distinct-subsequences-ii.java
@@ -1,16 +1,14 @@
 class Solution {
     public int distinctSubseqII(String s) {
         char[] chs = s.toCharArray();
-        int len = chs.length;
         int[] record = new int[26];
-
         int m = 1000000007;
         int all = 1;
-        for(int i = 0; i < len; i++){
-            int newCounts = all;
-            all = ((all + newCounts) % m - record[chs[i] - 'a'] % m + m) % m;
-            record[chs[i] - 'a'] = newCounts;
+        for(int i=0;i<chs.length;i++){
+            int newCount = all;
+            all = ((all+newCount) % m - record[chs[i]-'a'] % m + m) % m;
+            record[chs[i] - 'a'] = newCount;
         }
-        return all - 1;
+        return all-1;
     }
 }
