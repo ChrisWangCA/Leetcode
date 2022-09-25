904b3613216cb7daad431d6ec3645c1213e0a745
Chris Wang <100044659+ChrisWangCA@users.noreply.github.com>
2022-09-24 20:54:00 -0600

    Time: 260 ms (18.20%), Space: 105.6 MB (21.71%) - LeetHub

--- 502-ipo/502-ipo.java
+++ 502-ipo/502-ipo.java
@@ -1,21 +1,20 @@
 class Solution {
-    
-    public class Node{
+    class Node{
         int p;
         int c;
-        public Node(int p,int c){
+        Node(int p,int c){
             this.p = p;
             this.c = c;
         }
     }
-
+    
     public int findMaximizedCapital(int k, int w, int[] profits, int[] capital) {
         Node[] nodes = new Node[profits.length];
         for(int i=0;i<profits.length;i++){
             nodes[i] = new Node(profits[i],capital[i]);
         }
-        PriorityQueue<Node> small = new PriorityQueue<>((o1,o2)->o1.c - o2.c);
-        PriorityQueue<Node> big = new PriorityQueue<>((o1,o2)->o2.p - o1.p);
+        PriorityQueue<Node> small = new PriorityQueue<>((o1,o2)->o1.c-o2.c);
+        PriorityQueue<Node> big = new PriorityQueue<>((o1,o2)->o2.p-o1.p);
         for(int i=0;i<nodes.length;i++){
             small.add(nodes[i]);
         }
