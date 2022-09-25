019ba0da357820b64d93980e1e2d0788e2ec73ee
Chris Wang <100044659+ChrisWangCA@users.noreply.github.com>
2022-09-25 11:33:59 -0600

    Time: 4 ms (81.50%), Space: 45.7 MB (43.34%) - LeetHub

--- 51-n-queens/51-n-queens.java
+++ 51-n-queens/51-n-queens.java
@@ -6,24 +6,26 @@ class Solution {
         process(res,queen,0,n);
         return res;
     }
-    public void process(List<List<String>> res,int[] queen,int cur,int N){
-        if(cur>=N){
+    
+    public void process(List<List<String>> res,int[] queen,int cur,int n){
+        if(cur >= n){
             List<String> list = new ArrayList<>();
-            for(int i=0;i<N;i++){
-                char[] chs = new char[N];
+            for(int i=0;i<n;i++){
+                char[] chs = new char[n];
                 Arrays.fill(chs,'.');
                 chs[queen[i]] = 'Q';
                 list.add(new String(chs));
             }
             res.add(list);
         }
-        for(int j=0;j<N;j++){
+        for(int j=0;j<n;j++){
             if(isValid(queen,cur,j)){
                 queen[cur] = j;
-                process(res,queen,cur+1,N);
+                process(res,queen,cur+1,n);
             }
         }
     }
+    
     public boolean isValid(int[] queen,int cur,int j){
         for(int k=0;k<cur;k++){
             if(j == queen[k] || Math.abs(queen[k]-j) == Math.abs(cur - k)){
