a0c08fa79cca995b1135c8508bad02be75435068
Chris Wang <100044659+ChrisWangCA@users.noreply.github.com>
2022-09-25 22:03:33 -0600

    Time: 5 ms (14.92%), Space: 44.6 MB (62.83%) - LeetHub

--- 46-permutations/46-permutations.java
+++ 46-permutations/46-permutations.java
@@ -2,10 +2,10 @@ class Solution {
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> res = new ArrayList<>();
         List<Integer> list = new ArrayList<>();
-        dfs(nums,res,list);
+        process(nums,res,list);
         return res;
     }
-    public void dfs(int[] nums,List<List<Integer>> res,List<Integer> list){
+    public void process(int[] nums,List<List<Integer>> res,List<Integer> list){
         if(list.size() == nums.length){
             res.add(new ArrayList<>(list));
             return;
@@ -13,7 +13,7 @@ class Solution {
         for(int i=0;i<nums.length;i++){
             if(list.contains(nums[i])) continue;
             list.add(nums[i]);
-            dfs(nums,res,list);
+            process(nums,res,list);
             list.remove(list.size()-1);
         }
     }
