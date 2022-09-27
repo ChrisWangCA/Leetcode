33377e6bc56108c93cc4d8276f7843d38953452b
Chris Wang <100044659+ChrisWangCA@users.noreply.github.com>
2022-09-26 19:02:52 -0600

    Time: 2 ms (69.67%), Space: 42.4 MB (53.92%) - LeetHub

--- 91-decode-ways/91-decode-ways.java
+++ 91-decode-ways/91-decode-ways.java
@@ -5,13 +5,16 @@ class Solution {
         if(s == null || s.length() == 0) return 0;
         return process(s.toCharArray(),0,memo);
     }
+    
     public int process(char[] chs,int i,int[] memo){
         if(i == chs.length) return 1;
         if(chs[i] == '0') return 0;
-        if(memo[i] != -1) return memo[i];
+        if(memo[i] != -1){
+            return memo[i];
+        }
         if(chs[i] == '1'){
             int res = process(chs,i+1,memo);
-            if(i+1 < chs.length){
+            if(i+1<chs.length){
                 res += process(chs,i+2,memo);
             }
             memo[i] = res;
