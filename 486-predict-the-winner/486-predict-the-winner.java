093e2a5facc90db0ef69457a49b4fcbf0920aebe
Chris Wang <100044659+ChrisWangCA@users.noreply.github.com>
2022-09-30 14:40:41 -0600

    Time: 124 ms (22.62%), Space: 39.9 MB (88.40%) - LeetHub

--- 486-predict-the-winner/486-predict-the-winner.java
+++ 486-predict-the-winner/486-predict-the-winner.java
@@ -2,8 +2,8 @@ class Solution {
     public boolean PredictTheWinner(int[] nums) {
         return play(nums,0,nums.length-1) >= 0;
     }
-    public int play(int[] nums,int left,int right){
-        if(left>right) return 0;
+    public int play(int[] nums,int left,int right) {
+        if(left > right) return 0;
         int planA = nums[left] - play(nums,left+1,right);
         int planB = nums[right] - play(nums,left,right-1);
         return Math.max(planA,planB);
