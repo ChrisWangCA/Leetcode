60d195cb7c12287c9b6583f42c4bac98c7246b4c
Chris Wang <100044659+ChrisWangCA@users.noreply.github.com>
2022-09-29 19:57:32 -0600

    Time: 2 ms (100.00%), Space: 42.3 MB (93.68%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.java
+++ 695-max-area-of-island/695-max-area-of-island.java
@@ -1,26 +1,26 @@
 class Solution {
     public int maxAreaOfIsland(int[][] grid) {
-        if(grid==null || grid[0]==null) return 0;
-        int res=0;
-        int N=grid.length;
-        int M=grid[0].length;
+        if(grid == null || grid[0] == null) return 0;
+        int res = 0;
+        int N = grid.length;
+        int M = grid[0].length;
         for(int i=0;i<N;i++){
             for(int j=0;j<M;j++){
-                if(grid[i][j]==1){
-                    res=Math.max(res,infect(grid,i,j,N,M));
+                if(grid[i][j] == 1){
+                    res = Math.max(res,infect(grid,i,j,N,M));
                 }
             }
         }
         return res;
     }
-    public int infect(int[][] chs,int i,int j,int N,int M){
-        if(i<0 || i>=N || j<0 || j>=M || chs[i][j]!=1) return 0;
-        chs[i][j]=2;
-        int num=1;
-        num+=infect(chs,i+1,j,N,M);
-        num+=infect(chs,i-1,j,N,M);
-        num+=infect(chs,i,j+1,N,M);
-        num+=infect(chs,i,j-1,N,M);
+    public int infect(int[][] grid,int i,int j,int N,int M){
+        if(i < 0 || i >= N || j < 0 || j >= M || grid[i][j] != 1) return 0;
+        grid[i][j] = 2;
+        int num = 1;
+        num += infect(grid,i+1,j,N,M);
+        num += infect(grid,i-1,j,N,M);
+        num += infect(grid,i,j+1,N,M);
+        num += infect(grid,i,j-1,N,M);
         return num;
     }
 }
