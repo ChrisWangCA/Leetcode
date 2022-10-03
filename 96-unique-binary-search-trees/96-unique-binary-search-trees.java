4dd3cf689269f1a1fe46b54edd5c6cc78560f780
Chris Wang <100044659+ChrisWangCA@users.noreply.github.com>
2022-10-03 13:02:34 -0600

    Time: 0 ms (100.00%), Space: 41.3 MB (17.75%) - LeetHub

--- 96-unique-binary-search-trees/96-unique-binary-search-trees.java
+++ 96-unique-binary-search-trees/96-unique-binary-search-trees.java
@@ -5,7 +5,7 @@ class Solution {
         dp[0] = 1;
         for(int i=1;i<n+1;i++){
             for(int j=1;j<i+1;j++){
-                dp[i] += dp[j-1] * dp[i-j];
+                dp[i] += dp[j-1]*dp[i-j];
             }
         }
         return dp[n];
