e8a55dce628dfc2081f5f775bef5aef7121568be
Chris Wang <100044659+ChrisWangCA@users.noreply.github.com>
2022-10-04 13:29:23 -0600

    Time: 0 ms (100.00%), Space: 41.2 MB (79.24%) - LeetHub

--- 24-swap-nodes-in-pairs/24-swap-nodes-in-pairs.java
+++ 24-swap-nodes-in-pairs/24-swap-nodes-in-pairs.java
@@ -15,7 +15,7 @@ class Solution {
         while(cur.next!=null && cur.next.next!=null){
             ListNode temp1 = cur.next;
             ListNode temp2 = cur.next.next.next;
-            cur.next = cur.next.next;
+            cur.next = temp1.next;
             cur.next.next = temp1;
             temp1.next = temp2;
             cur = cur.next.next;
