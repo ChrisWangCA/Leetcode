d1edd65f6722d38580e7a3d911596bd8d415013b
Chris Wang <100044659+ChrisWangCA@users.noreply.github.com>
2022-10-06 10:35:39 -0600

    Time: 173 ms (80.47%), Space: 42.5 MB (90.93%) - LeetHub

--- 454-4sum-ii/454-4sum-ii.java
+++ 454-4sum-ii/454-4sum-ii.java
@@ -1,7 +1,7 @@
 class Solution {
     public int fourSumCount(int[] nums1, int[] nums2, int[] nums3, int[] nums4) {
         Map<Integer,Integer> map = new HashMap<>();
-        int res=0;
+        int res = 0;
         for(int i:nums1){
             for(int j:nums2){
                 int temp = i+j;
@@ -14,9 +14,9 @@ class Solution {
         }
         for(int i:nums3){
             for(int j:nums4){
-                int temp=i+j;
-                if(map.containsKey(0-temp)){
-                    res+=map.get(0-temp);
+                int temp = i+j;
+                if(map.containsKey(0 - temp)){
+                    res += map.get(0 - temp);
                 }
             }
         }
