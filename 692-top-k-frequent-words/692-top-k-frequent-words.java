02be6acd8548150dcb32bc7db86c9af3f2522b4e
Chris Wang <100044659+ChrisWangCA@users.noreply.github.com>
2022-10-09 12:21:04 -0600

    Time: 11 ms (62.70%), Space: 45.1 MB (60.70%) - LeetHub

--- 692-top-k-frequent-words/692-top-k-frequent-words.java
+++ 692-top-k-frequent-words/692-top-k-frequent-words.java
@@ -2,8 +2,8 @@ class Solution {
     public List<String> topKFrequent(String[] words, int k) {
         List<String> res = new ArrayList<>();
         HashMap<String,Integer> map = new HashMap<>();
-        for(String word:words){
-            map.put(word,map.getOrDefault(word,0)+1);
+        for(String s:words){
+            map.put(s,map.getOrDefault(s,0)+1);
         }
         for(Map.Entry<String,Integer> entry:map.entrySet()){
             res.add(entry.getKey());
@@ -11,7 +11,7 @@ class Solution {
         Collections.sort(res,new Comparator<String>(){
             public int compare(String o1,String o2){
                 return map.get(o1) == map.get(o2) ? 
-                    o1.compareTo(o2):map.get(o2) - map.get(o1);
+                    o1.compareTo(o2) : map.get(o2) - map.get(o1);
             }
         });
         return res.subList(0,k);
