9e71c6ca686bc9253b9c56ab4224654e2ea7a9ab
Chris Wang <100044659+ChrisWangCA@users.noreply.github.com>
2022-10-09 12:25:39 -0600

    Time: 3 ms (23.91%), Space: 46.2 MB (23.91%) - LeetHub

--- 517-super-washing-machines/517-super-washing-machines.java
+++ 517-super-washing-machines/517-super-washing-machines.java
@@ -3,9 +3,9 @@ class Solution {
         if(machines == null || machines.length == 0) return -1;
         int size = machines.length;
         int sum = 0;
-        int res = 0;
         int leftSum = 0;
-        for(int i=0;i<size;i++){
+        int res = 0;
+        for(int i=0;i<machines.length;i++){
             sum += machines[i];
         }
         if(sum % size != 0) return -1;
@@ -14,7 +14,7 @@ class Solution {
             int leftRest = i * avg - leftSum;
             int rightRest = (size - i - 1) * avg - (sum - leftSum - machines[i]);
             if(leftRest > 0 && rightRest > 0){
-                res = Math.max(res,Math.abs(leftRest)+Math.abs(rightRest));
+                res = Math.max(res,Math.abs(leftRest) + Math.abs(rightRest));
             }else{
                 res = Math.max(res,Math.max(Math.abs(leftRest),Math.abs(rightRest)));
             }
