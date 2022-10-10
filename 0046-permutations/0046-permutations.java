05051954f4c47a5271ae37ce731bcb50b04ef47c
Chris Wang <100044659+ChrisWangCA@users.noreply.github.com>
2022-10-10 11:04:05 -0600

    Time: 3 ms (50.69%), Space: 45 MB (28.54%) - LeetHub

---
+++ 0046-permutations/0046-permutations.java
@@ -0,0 +1,21 @@
+class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        List<Integer> list = new ArrayList<>();
+        process(nums,res,list);
+        return res;
+    }
+    
+    public void process(int[] nums,List<List<Integer>> res,List<Integer> list){
+        if(list.size() == nums.length){
+            res.add(new ArrayList<>(list));
+            return;
+        }
+        for(int i=0;i<nums.length;i++){
+            if(list.contains(nums[i])) continue;
+            list.add(nums[i]);
+            process(nums,res,list);
+            list.remove(list.size()-1);
+        }
+    }
+}
