a962624dde8e1d720a02f43d7c459457be5b35de
Chris Wang <100044659+ChrisWangCA@users.noreply.github.com>
2022-10-10 10:54:28 -0600

    Time: 3 ms (97.48%), Space: 50.6 MB (95.95%) - LeetHub

---
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int numIslands(char[][] grid) {
+        if(grid == null || grid[0] == null) return 0;
+        int row = grid.length;
+        int col = grid[0].length;
+        int res = 0;
+        for(int i=0;i<row;i++){
+            for(int j=0;j<col;j++){
+                if(grid[i][j] == '1'){
+                    res ++;
+                    infect(grid,i,j,row,col);
+                }
+            }
+        }
+        return res;
+    }
+    
+    public void infect(char[][] grid,int i,int j,int row,int col){
+        if(i < 0 || i >= row || j < 0 || j >= col || grid[i][j] != '1') return;
+        grid[i][j] = '2';
+        infect(grid,i+1,j,row,col);
+        infect(grid,i-1,j,row,col);
+        infect(grid,i,j+1,row,col);
+        infect(grid,i,j-1,row,col);
+    }
+}
