d801986b1c2f3a6a2a66d859f946cf45ca90dc38
Chris Wang <100044659+ChrisWangCA@users.noreply.github.com>
2022-10-13 11:31:39 -0600

    Time: 1 ms (93.64%), Space: 41.9 MB (92.41%) - LeetHub

---
+++ 0337-house-robber-iii/0337-house-robber-iii.java
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    
+    public class Info{
+        int no;
+        int yes;
+        Info(int n,int y){
+            this.no = n;
+            this.yes = y;
+        }
+    }
+    
+    public int rob(TreeNode root) {
+        Info res = process(root);
+        return Math.max(res.no,res.yes);
+    }
+    
+    public Info process(TreeNode root){
+        if(root == null) return new Info(0,0);
+        Info left = process(root.left);
+        Info right = process(root.right);
+        int planA = Math.max(left.no,left.yes) + Math.max(right.no,right.yes);
+        int planB = root.val + left.no + right.no;
+        return new Info(planA,planB);
+    }
+}
