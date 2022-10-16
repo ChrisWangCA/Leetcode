74cb602ef9e949d330b4c0d040afcb80c032baa7
Chris Wang <100044659+ChrisWangCA@users.noreply.github.com>
2022-10-16 10:44:08 -0600

    Time: 1 ms (75.70%), Space: 41.7 MB (71.71%) - LeetHub

---
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean checkValidString(String s) {
+        Stack<Integer> stack = new Stack<>();
+        Stack<Integer> starStack = new Stack<>();
+        char[] chs = s.toCharArray();
+        for(int i=0;i<chs.length;i++){
+            if(chs[i] == '('){
+                stack.push(i);
+            }else if(chs[i] == '*'){
+                starStack.push(i);
+            }else {
+                if(!stack.isEmpty()){
+                    stack.pop();
+                }else if(!starStack.isEmpty()){
+                    starStack.pop();
+                }else{
+                    return false;
+                }
+            }
+        }
+        while(!stack.isEmpty() && !starStack.isEmpty()){
+            int i = stack.pop();
+            int j = starStack.pop();
+            if(i > j) return false;
+        }
+        return stack.isEmpty();
+    }
+}
