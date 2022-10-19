43720f2d83b6a70b2524acc0e9d0b4a5f5ade808
Chris Wang <100044659+ChrisWangCA@users.noreply.github.com>
2022-10-19 13:56:04 -0600

    Time: 1 ms (76.46%), Space: 41.7 MB (68.12%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.java
+++ 0796-rotate-string/0796-rotate-string.java
@@ -1,5 +1,5 @@
 class Solution {
     public boolean rotateString(String s, String goal) {
-        return s.length() == goal.length() && (s + s).contains(goal);
+        return s.length() == goal.length() && (s+s).contains(goal);
     }
 }
