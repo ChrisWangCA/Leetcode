c83fbf9b3744c20479ba5826b0bcbf731afb5861
Chris Wang <100044659+ChrisWangCA@users.noreply.github.com>
2022-10-19 14:04:05 -0600

    Time: 0 ms (100.00%), Space: 45.5 MB (55.65%) - LeetHub

---
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.java
@@ -0,0 +1,46 @@
+/*
+// Definition for a Node.
+class Node {
+    int val;
+    Node next;
+    Node random;
+
+    public Node(int val) {
+        this.val = val;
+        this.next = null;
+        this.random = null;
+    }
+}
+*/
+
+class Solution {
+    public Node copyRandomList(Node head) {
+        if(head == null) return head;
+        Node cur = head;
+        Node next = null;
+        while(cur!=null){
+            next = cur.next;
+            cur.next = new Node(cur.val);
+            cur.next.next = next;
+            cur = next;
+        }
+        cur = head;
+        Node copy = null;
+        while(cur!=null){
+            next = cur.next.next;
+            copy = cur.next;
+            copy.random = cur.random != null ? cur.random.next : null;
+            cur = next;
+        }
+        Node res = head.next;
+        cur = head;
+        while(cur != null){
+            next = cur.next.next;
+            copy = cur.next;
+            cur.next = next;
+            copy.next = next != null ? next.next : null;
+            cur = next;
+        }
+        return res;
+    }
+}
