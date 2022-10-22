ddb68c0d46553b109e14cbf3aefc4ede0d082ac3
Chris Wang <100044659+ChrisWangCA@users.noreply.github.com>
2022-10-22 12:55:13 -0600

    Time: 3 ms (87.86%), Space: 46.7 MB (79.19%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.java
+++ 0695-max-area-of-island/0695-max-area-of-island.java
@@ -1,27 +1,27 @@
 class Solution {
     public int maxAreaOfIsland(int[][] grid) {
+        if(grid == null || grid[0] == null) return 0;
+        int row = grid.length;
+        int col = grid[0].length;
         int res = 0;
-        if(grid == null || grid[0] == null)  return res;
-        int N = grid.length;
-        int M = grid[0].length;
-        for(int i=0;i<N;i++){
-            for(int j=0;j<M;j++){
+        for(int i=0;i<row;i++){
+            for(int j=0;j<col;j++){
                 if(grid[i][j] == 1){
-                    res = Math.max(res,infect(grid,i,j,N,M));
+                    res = Math.max(res,infect(grid,i,j,row,col));
                 }
             }
         }
         return res;
     }
     
-    public int infect(int[][] grid,int i,int j,int N,int M){
-        if(i < 0 || i >= N || j < 0 || j >= M || grid[i][j] != 1) return 0;
+    public int infect(int[][] grid,int i,int j,int row,int col){
+        if(i < 0 || i >= row || j < 0 || j >= col || grid[i][j] != 1) return 0;
         grid[i][j] = 2;
         int num = 1;
-        num += infect(grid,i+1,j,N,M);
-        num += infect(grid,i-1,j,N,M);
-        num += infect(grid,i,j+1,N,M);
-        num += infect(grid,i,j-1,N,M);
+        num += infect(grid,i+1,j,row,col);
+        num += infect(grid,i-1,j,row,col);
+        num += infect(grid,i,j+1,row,col);
+        num += infect(grid,i,j-1,row,col);
         return num;
     }
 }
