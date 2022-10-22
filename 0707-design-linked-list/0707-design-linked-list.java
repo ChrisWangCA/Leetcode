65212bb12954568883bea67f8824a7bb1b804f8a
Chris Wang <100044659+ChrisWangCA@users.noreply.github.com>
2022-10-22 13:08:37 -0600

    Time: 18 ms (34.58%), Space: 50.1 MB (81.47%) - LeetHub

---
+++ 0707-design-linked-list/0707-design-linked-list.java
@@ -0,0 +1,80 @@
+class MyLinkedList {
+    
+    int N;
+    ListNode head;
+    
+    public class ListNode{
+        ListNode next;
+        int value;
+        ListNode(){};
+        ListNode(int value){this.value = value;}
+    }
+    
+    public MyLinkedList() {
+        N = 0;
+        head = new ListNode(0);
+    }
+    
+    public int get(int index) {
+        if(index < 0 || index >= N) return -1;
+        ListNode cur = head;
+        for(int i=0;i<=index;i++){
+            cur = cur.next;
+        }
+        return cur.value;
+    }
+    
+    public void addAtHead(int val) {
+        ListNode newNode = new ListNode(val);
+        newNode.next = head.next;
+        head.next = newNode;
+        N++;
+    }
+    
+    public void addAtTail(int val) {
+        ListNode newNode = new ListNode(val);
+        ListNode cur = head;
+        if(head == null){
+            head = newNode;
+        }else{
+            while(cur.next != null){
+                cur = cur.next;
+            }
+            cur.next = newNode;
+        }
+        N++;
+    }
+    
+    public void addAtIndex(int index, int val) {
+        if(index < 0) index = 0;
+        if(index > N) return;
+        ListNode newNode = new ListNode(val);
+        ListNode cur = head;
+        for(int i=0;i<index;i++){
+            cur = cur.next;
+        }
+        newNode.next = cur.next;
+        cur.next = newNode;
+        N++;
+    }
+    
+    public void deleteAtIndex(int index) {
+        if(index < 0 || index >= N) return;
+        ListNode cur = head;
+        for(int i=0;i<index;i++){
+            cur = cur.next;
+        }
+        cur.next = cur.next.next;
+        N--;
+    }
+}
+
+/**
+ * Your MyLinkedList object will be instantiated and called as such:
+ * MyLinkedList obj = new MyLinkedList();
+ * int param_1 = obj.get(index);
+ * obj.addAtHead(val);
+ * obj.addAtTail(val);
+ * obj.addAtIndex(index,val);
+ * obj.deleteAtIndex(index);
+ */
