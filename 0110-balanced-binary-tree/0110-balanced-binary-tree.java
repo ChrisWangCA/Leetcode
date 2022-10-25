5cde2499d0c5ec186d4a7f0c3badc68a398c0949
Chris Wang <100044659+ChrisWangCA@users.noreply.github.com>
2022-10-25 08:57:58 -0600

    Time: 1 ms (99.16%), Space: 44.3 MB (54.81%) - LeetHub

---
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.java
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isBalanced(TreeNode root) {
+        if(root == null) return true;
+        return Math.abs(process(root.left) - process(root.right)) <= 1 &&
+            isBalanced(root.left) && isBalanced(root.right);
+    }
+    
+    public int process(TreeNode root){
+        if(root == null) return 0;
+        return Math.max(process(root.left),process(root.right))+1;
+    }
+}
