1ca2f1a7686a77444007c6f4d9d44f893a964a4c
Chris Wang <100044659+ChrisWangCA@users.noreply.github.com>
2022-10-31 19:24:11 -0600

    Time: 1 ms (98.29%), Space: 40.4 MB (94.98%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.java
+++ 0091-decode-ways/0091-decode-ways.java
@@ -2,31 +2,29 @@ class Solution {
     public int numDecodings(String s) {
         int[] memo = new int[s.length()];
         Arrays.fill(memo,-1);
-        return process(s.toCharArray(),0,memo);
+        return process(s.toCharArray(),memo,0);
     }
-    public int process(char[] chs, int i,int[] memo){
-        if(i == memo.length) return 1;
+    public int process(char[] chs,int[] memo,int i){
+        if(i == chs.length) return 1;
         if(chs[i] == '0') return 0;
-        if(memo[i] != -1){
-            return memo[i];
-        }
+        if(memo[i] != -1) return memo[i];
         if(chs[i] == '1'){
-            int res = process(chs,i+1,memo);
+            int res = process(chs,memo,i+1);
             if(i + 1 < chs.length){
-                res += process(chs,i+2,memo);
+                res += process(chs,memo,i+2);
             }
             memo[i] = res;
             return res;
         }
         if(chs[i] == '2'){
-            int res = process(chs,i+1,memo);
+            int res = process(chs,memo,i+1);
             if(i + 1 < chs.length && chs[i+1] >= '0' && chs[i+1] <= '6'){
-                res += process(chs,i+2,memo);
+                res += process(chs,memo,i+2);
             }
             memo[i] = res;
             return res;
         }
-        int res = process(chs,i+1,memo);
+        int res = process(chs,memo,i+1);
         memo[i] = res;
         return res;
     }
