46be94d2d9a6e16fdd42e7dd678573bf1ec018ec
Chris Wang <100044659+ChrisWangCA@users.noreply.github.com>
2022-11-11 11:40:36 -0700

    Time: 5 ms (78.98%), Space: 57.6 MB (33.54%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,26 +1,26 @@
 class Solution {
     public int numIslands(char[][] grid) {
-        if(grid == null || grid[0] == null) return 0;
         int res = 0;
-        int row = grid.length;
-        int col = grid[0].length;
-        for(int i=0;i<row;i++){
-            for(int j=0;j<col;j++){
+        if(grid == null || grid[0] == null) return res;
+        int N = grid.length;
+        int M = grid[0].length;
+        for(int i=0;i<N;i++){
+            for(int j=0;j<M;j++){
                 if(grid[i][j] == '1'){
-                    res ++;
-                    infect(grid,i,j,row,col);
+                    res++;
+                    infect(grid,i,j,N,M);
                 }
             }
         }
         return res;
     }
     
-    public void infect(char[][] grid,int i,int j,int row,int col){
-        if(i < 0 || i >= row || j < 0 || j >= col || grid[i][j] != '1') return;
+    public void infect(char[][] grid,int i,int j,int N,int M){
+        if(i < 0 || i >= N || j < 0 || j >= M || grid[i][j] != '1') return;
         grid[i][j] = '2';
-        infect(grid,i+1,j,row,col);
-        infect(grid,i-1,j,row,col);
-        infect(grid,i,j+1,row,col);
-        infect(grid,i,j-1,row,col);
+        infect(grid,i+1,j,N,M);
+        infect(grid,i-1,j,N,M);
+        infect(grid,i,j+1,N,M);
+        infect(grid,i,j-1,N,M);
     }
 }
