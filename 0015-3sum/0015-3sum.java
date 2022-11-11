81fc047d93f559a48974be81c9ff88cc6c5db999
Chris Wang <100044659+ChrisWangCA@users.noreply.github.com>
2022-11-11 11:56:59 -0700

    Time: 30 ms (82.34%), Space: 58.9 MB (60.71%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -5,20 +5,20 @@ class Solution {
         for(int i=0;i<nums.length;i++){
             if(nums[i] > 0) return res;
             if(i > 0 && nums[i] == nums[i-1]) continue;
-            int left = i+1;
+            int left = i + 1;
             int right = nums.length-1;
             while(right > left){
                 int sum = nums[i] + nums[left] + nums[right];
                 if(sum > 0){
-                    right--;
+                    right --;
                 }else if(sum < 0){
-                    left++;
+                    left ++;
                 }else{
                     res.add(Arrays.asList(nums[i],nums[left],nums[right]));
                     while(right > left && nums[right] == nums[right-1]) right--;
-                    while(right > left && nums[left] == nums[left+1]) left++;
-                    right--;
-                    left++;
+                    while(right > left && nums[left] == nums[left+1]) left ++;
+                    right --;
+                    left ++;
                 }
             }
         }
