eb00265282f2152115b572d2f318f6ee3eb9ade8
Chris Wang <100044659+ChrisWangCA@users.noreply.github.com>
2022-11-16 08:37:32 -0700

    Time: 2 ms (58.90%), Space: 43.9 MB (51.06%) - LeetHub

--- 0513-find-bottom-left-tree-value/0513-find-bottom-left-tree-value.java
+++ 0513-find-bottom-left-tree-value/0513-find-bottom-left-tree-value.java
@@ -23,15 +23,9 @@ class Solution {
             int len = queue.size();
             for(int i=0;i<len;i++){
                 TreeNode temp = queue.poll();
-                if(temp.left != null){
-                    queue.offer(temp.left);
-                }
-                if(temp.right != null){
-                    queue.offer(temp.right);
-                }
-                if(i == 0){
-                    res = temp.val;
-                }
+                if(temp.left != null) queue.offer(temp.left);
+                if(temp.right != null) queue.offer(temp.right);
+                if(i == 0) res = temp.val;
             }
         }
         return res;
