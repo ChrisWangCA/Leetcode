1b408b1453e23602aaa3aa74de7f83abba641da5
Chris Wang <100044659+ChrisWangCA@users.noreply.github.com>
2022-11-18 16:21:32 -0700

    Time: 2 ms (89.06%), Space: 43.4 MB (66.73%) - LeetHub

--- 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.java
+++ 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.java
@@ -17,29 +17,29 @@ class Solution {
     public int widthOfBinaryTree(TreeNode root) {
         int max = 1;
         if(root == null) return 0;
-        root.val = 1;
         int next = 0;
-        Deque<TreeNode> queue = new LinkedList<>();
+        root.val = 1;
+        Deque<TreeNode> queue = new ArrayDeque<>();
         queue.offer(root);
         while(!queue.isEmpty()){
             int len = queue.size();
             while(len > 0){
-                len --;
+                len--;
                 TreeNode temp = queue.poll();
-                if(temp.left!=null){
+                if(temp.left != null){
                     temp.left.val = temp.val * 2;
                     queue.offer(temp.left);
                     next++;
                 }
-                if(temp.right!=null){
+                if(temp.right != null){
                     temp.right.val = temp.val * 2 + 1;
                     queue.offer(temp.right);
                     next++;
                 }
                 if(len == 0){
-                    if(next != 0){
-                        max = Math.max(max,queue.peekLast().val - queue.peekFirst().val+1);
-                        next = 0;
+                    if(next!=0){
+                        max = Math.max(max,queue.peekLast().val - queue.peekFirst().val + 1);
+                        next=0;
                     }
                 }
             }
