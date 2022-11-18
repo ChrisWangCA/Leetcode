bf5700c86e2cb16599e6acbd4b2769017fb84691
Chris Wang <100044659+ChrisWangCA@users.noreply.github.com>
2022-11-18 15:52:17 -0700

    Time: 2 ms (95.48%), Space: 43.4 MB (46.90%) - LeetHub

--- 0032-longest-valid-parentheses/0032-longest-valid-parentheses.java
+++ 0032-longest-valid-parentheses/0032-longest-valid-parentheses.java
@@ -1,19 +1,18 @@
 class Solution {
     public int longestValidParentheses(String s) {
-        if(s == null || s.equals("")) return 0;
+        if(s.equals("") || s == null) return 0;
         char[] chs = s.toCharArray();
         int[] dp = new int[chs.length];
         int pre = 0;
         int res = 0;
         for(int i=1;i<chs.length;i++){
             if(chs[i] == ')'){
-                pre = i - dp[i - 1] - 1;
-                 if(pre >= 0 && chs[pre] == '('){
-                dp[i] = dp[i-1] + 2 + (pre > 0 ? dp[pre-1]:0);
-            }
+                pre = i - dp[i-1] - 1;
+                if(pre >= 0 && chs[pre] == '('){
+                    dp[i] = dp[i-1] + 2 + (pre > 0 ? dp[pre-1]:0);
+                }
                 res = Math.max(res,dp[i]);
             }
-            
         }
         return res;
     }
