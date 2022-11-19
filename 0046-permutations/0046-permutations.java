c9f0ec214e3be03d7cec95b01f552a7c232b226a
Chris Wang <100044659+ChrisWangCA@users.noreply.github.com>
2022-11-19 15:23:09 -0700

    Time: 3 ms (52.36%), Space: 44.5 MB (66.96%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -5,7 +5,8 @@ class Solution {
         process(nums,res,list);
         return res;
     }
-    public void process(int[] nums,List<List<Integer>> res, List<Integer> list){
+    
+    public void process(int[] nums,List<List<Integer>> res,List<Integer> list){
         if(list.size() == nums.length){
             res.add(new ArrayList<>(list));
             return;
