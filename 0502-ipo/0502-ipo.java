d9139bd27a48d1668a04b824772000b9c7858f3e
Chris Wang <100044659+ChrisWangCA@users.noreply.github.com>
2022-11-23 09:07:11 -0700

    Time: 229 ms (51.95%), Space: 105.1 MB (30.80%) - LeetHub

---
+++ 0502-ipo/0502-ipo.java
@@ -0,0 +1,31 @@
+class Solution {
+    
+    class Info{
+        int p;
+        int c;
+        Info(int p,int c){
+            this.p = p;
+            this.c = c;
+        }
+    }
+    
+    public int findMaximizedCapital(int k, int w, int[] profits, int[] capital) {
+        Info[] record = new Info[profits.length];
+        for(int i=0;i<profits.length;i++){
+            record[i] = new Info(profits[i],capital[i]);
+        }
+        PriorityQueue<Info> small = new PriorityQueue<>((o1,o2)->o1.c-o2.c);
+        PriorityQueue<Info> big = new PriorityQueue<>((o1,o2)->o2.p-o1.p);
+        for(int i=0;i<profits.length;i++){
+            small.offer(record[i]);
+        }
+        for(int i=0;i<k;i++){
+            while(!small.isEmpty() && small.peek().c <= w){
+                big.offer(small.poll());
+            }
+            if(big.isEmpty()) return w;
+            w += big.poll().p;
+        }
+        return w;
+    }
+}
