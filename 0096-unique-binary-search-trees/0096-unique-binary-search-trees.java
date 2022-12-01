1b32b91377317603a55dfe856b8bc3bb3fedea68
Chris Wang <100044659+ChrisWangCA@users.noreply.github.com>
2022-12-01 15:06:23 -0700

    Time: 0 ms (100.00%), Space: 39.2 MB (83.48%) - LeetHub

--- 0096-unique-binary-search-trees/0096-unique-binary-search-trees.java
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.java
@@ -1,7 +1,7 @@
 class Solution {
     public int numTrees(int n) {
-        if(n < 2) return 1;
         int[] dp = new int[n+1];
+        if(n < 2) return 1;
         dp[0] = 1;
         for(int i=1;i<n+1;i++){
             for(int j=1;j<i+1;j++){
