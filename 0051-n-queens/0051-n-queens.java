8c59c53302e9f81e35f617a3832f68b247c51080
Chris Wang <100044659+ChrisWangCA@users.noreply.github.com>
2022-12-01 14:47:33 -0700

    Time: 2 ms (98.04%), Space: 42.1 MB (98.01%) - LeetHub

--- 0051-n-queens/0051-n-queens.java
+++ 0051-n-queens/0051-n-queens.java
@@ -1,34 +1,33 @@
 class Solution {
     public List<List<String>> solveNQueens(int n) {
-        int[] record = new int[n];
-        Arrays.fill(record,0);
+        int[] records = new int[n];
+        Arrays.fill(records,0);
         List<List<String>> res = new ArrayList<>();
-        process(res,record,0,n);
+        process(res,records,0,n);
         return res;
     }
     
-    public void process(List<List<String>> res,int[] record,int cur,int n){
+    public void process(List<List<String>> res,int[] records,int cur,int n){
         if(cur >= n){
             List<String> list = new ArrayList<>();
             for(int i=0;i<n;i++){
                 char[] chs = new char[n];
                 Arrays.fill(chs,'.');
-                chs[record[i]] = 'Q';
+                chs[records[i]] = 'Q';
                 list.add(new String(chs));
             }
             res.add(list);
         }
         for(int j=0;j<n;j++){
-            if(isValid(record,cur,j)){
-                record[cur] = j;
-                process(res,record,cur+1,n);
+            if(isValid(records,cur,j)){
+                records[cur] = j;
+                process(res,records,cur+1,n);
             }
         }
-    }
-    
-    public boolean isValid(int[] record,int cur,int j){
+    }  
+    public boolean isValid(int[] records,int cur,int j){
         for(int k=0;k<cur;k++){
-            if(record[k] == j || Math.abs(record[k] - j) == Math.abs(cur - k)){
+            if(records[k] == j || Math.abs(records[k] - j) == Math.abs(cur - k)){
                 return false;
             }
         }
