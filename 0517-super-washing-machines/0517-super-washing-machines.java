bafdb05bf3c53e82396ca7c7541782dcf00f2204
Chris Wang <100044659+ChrisWangCA@users.noreply.github.com>
2022-12-02 12:20:48 -0700

    Time: 1 ms (93.33%), Space: 42.9 MB (84.44%) - LeetHub

--- 0517-super-washing-machines/0517-super-washing-machines.java
+++ 0517-super-washing-machines/0517-super-washing-machines.java
@@ -2,15 +2,15 @@ class Solution {
     public int findMinMoves(int[] machines) {
         if(machines == null || machines.length == 0) return -1;
         int len = machines.length;
-        int leftSum = 0;
         int sum = 0;
+        int leftSum = 0;
         int res = 0;
-        for(int i=0;i<machines.length;i++){
-            sum += machines[i];
+        for(int i:machines){
+            sum += i;
         }
         if(sum % len != 0) return -1;
         int avg = sum / len;
-        for(int i=0;i<machines.length;i++){
+        for(int i=0;i<len;i++){
             int leftRest = i * avg - leftSum;
             int rightRest = (len - i - 1) * avg - (sum - leftSum - machines[i]);
             if(leftRest > 0 && rightRest > 0){
