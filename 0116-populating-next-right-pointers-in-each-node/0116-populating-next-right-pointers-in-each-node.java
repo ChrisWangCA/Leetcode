ef0728f41ce22a73a4eb79a63dbf7d80afaecbbb
Chris Wang <100044659+ChrisWangCA@users.noreply.github.com>
2023-05-29 16:06:25 +0800

    Time: 3 ms (42.34%), Space: 44.1 MB (5.39%) - LeetHub

--- 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.java
+++ 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.java
@@ -30,11 +30,15 @@ class Solution {
             int len = queue.size();
             for(int i=0;i<len;i++){
                 Node temp = queue.poll();
-                if(temp.left != null) queue.offer(temp.left);
-                if(temp.right != null) queue.offer(temp.right);
-                if(i != len-1){
-                    temp.next = queue.peek();
+                if(temp.left != null){
+                    queue.offer(temp.left);
                 }
+                if(temp.right != null){
+                    queue.offer(temp.right);
+                }
+                 if(i!=len-1){
+                    temp.next = queue.peek();
+              }
             }
         }
         return root;
