709c56eda1662f3df07f6274c29cf146e0205db4
Chris Wang <100044659+ChrisWangCA@users.noreply.github.com>
2023-05-31 15:03:20 +0800

    Time: 7 ms (99.82%), Space: 44.7 MB (12.00%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
@@ -13,6 +13,6 @@ class Solution {
         TreeNode left = lowestCommonAncestor(root.left,p,q);
         TreeNode right = lowestCommonAncestor(root.right,p,q);
         if(left != null && right != null) return root;
-        return left != null ? left : right;
+        return left != null ? left:right;
     }
 }
