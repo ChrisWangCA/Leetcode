f10d4dba03afc03a921a5dc436d8af2a743aaafb
Chris Wang <100044659+ChrisWangCA@users.noreply.github.com>
2023-06-02 14:20:43 +0800

    Time: 17 ms (56.66%), Space: 45.1 MB (36.94%) - LeetHub

--- 0297-serialize-and-deserialize-binary-tree/0297-serialize-and-deserialize-binary-tree.java
+++ 0297-serialize-and-deserialize-binary-tree/0297-serialize-and-deserialize-binary-tree.java
@@ -12,26 +12,26 @@ public class Codec {
     // Encodes a tree to a single string.
     public String serialize(TreeNode root) {
         if(root == null) return "#_";
-        String res = root.val + "_";
-        res += serialize(root.left);
-        res += serialize(root.right);
-        return res;
+        String str = root.val + "_";
+        str += serialize(root.left);
+        str += serialize(root.right);
+        return str;
     }
 
     // Decodes your encoded data to tree.
     public TreeNode deserialize(String data) {
-        String[] res = data.split("_");
+        String[] str = data.split("_");
         Deque<String> queue = new ArrayDeque<>();
-        for(int i=0;i<res.length;i++){
-            queue.offer(res[i]);
+        for(int i=0;i<str.length;i++){
+            queue.offer(str[i]);
         }
         return deserialize(queue);
     }
     
     public TreeNode deserialize(Deque<String> queue){
-        String temp = queue.poll();
-        if(temp.equals("#")) return null;
-        TreeNode root = new TreeNode(Integer.valueOf(temp));
+        String str = queue.poll();
+        if(str.equals("#")) return null;
+        TreeNode root = new TreeNode(Integer.valueOf(str));
         root.left = deserialize(queue);
         root.right = deserialize(queue);
         return root;
