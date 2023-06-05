00190d822b5f5c3e74ea15266d6992951f474515
Chris Wang <100044659+ChrisWangCA@users.noreply.github.com>
2023-06-05 18:36:20 +0800

    Time: 0 ms (100.00%), Space: 40.7 MB (54.73%) - LeetHub

--- 0059-spiral-matrix-ii/0059-spiral-matrix-ii.java
+++ 0059-spiral-matrix-ii/0059-spiral-matrix-ii.java
@@ -5,22 +5,22 @@ class Solution {
         int start = 0;
         int count = 1;
         int i,j;
-        while(loop++ < n / 2){
+        while(loop++ < n/2){
             for(j = start;j<n-loop;j++){
                 res[start][j] = count++;
             }
-            for(i = start;i<n-loop;i++){
+            for(i=start;i<n-loop;i++){
+                res[i][j]=count++;
+            }
+            for(;j>=loop;j--){
                 res[i][j] = count++;
             }
-            for(;j >= loop;j--){
-                res[i][j] = count++;
-            }
-            for(;i >= loop;i--){
+            for(;i>=loop;i--){
                 res[i][j] = count++;
             }
             start++;
         }
-        if(n % 2 == 1){
+        if(n%2==1){
             res[start][start] = count;
         }
         return res;
