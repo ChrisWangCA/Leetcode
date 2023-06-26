b7422b42e45399c7e123d6fffd154d47cfd93cca
Chris Wang <100044659+ChrisWangCA@users.noreply.github.com>
2023-06-25 20:40:56 -0600

    Time: 1 ms (82.60%), Space: 40.4 MB (98.11%) - LeetHub

--- 0844-backspace-string-compare/0844-backspace-string-compare.java
+++ 0844-backspace-string-compare/0844-backspace-string-compare.java
@@ -1,6 +1,7 @@
 class Solution {
     public boolean backspaceCompare(String s, String t) {
         char[] chs = s.toCharArray();
+        char[] cht = t.toCharArray();
         int slow = 0;
         for(int i=0;i<chs.length;i++){
             if(chs[i] == '#'){
@@ -11,7 +12,6 @@ class Solution {
         }
         String s1 = new String(chs,0,slow);
         slow = 0;
-        char[] cht = t.toCharArray();
         for(int i=0;i<cht.length;i++){
             if(cht[i] == '#'){
                 slow = Math.max(0,slow-1);
