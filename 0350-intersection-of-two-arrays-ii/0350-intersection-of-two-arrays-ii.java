636f5f5f1c47cd9836df7e1e8ba3156cac3b040a
Chris Wang <100044659+ChrisWangCA@users.noreply.github.com>
2023-06-30 19:51:10 -0600

    Time: 3 ms (58.70%), Space: 44 MB (7.36%) - LeetHub

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.java
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.java
@@ -1,17 +1,16 @@
 class Solution {
     public int[] intersect(int[] nums1, int[] nums2) {
-        int[] res = new int[1001];
         List<Integer> list = new ArrayList<>();
-        for(int i:nums1){
-            res[i] +=1;
+        int[] res = new int[1001];
+        for(int c:nums1){
+            res[c]+=1;
         }
-        for(int j:nums2){
-            if(res[j] > 0){
-                list.add(j);
-                res[j]-=1;
+        for(int c:nums2){
+            if(res[c] > 0){
+                list.add(c);
+                res[c]--;
             }
         }
-        
         return list.stream().mapToInt(s->s).toArray();
     }
 }
