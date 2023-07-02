54fec735588f09023f9e64b07da5c35c15d8d058
Chris Wang <100044659+ChrisWangCA@users.noreply.github.com>
2023-07-02 17:59:50 -0600

    Time: 0 ms (100.00%), Space: 43.6 MB (65.36%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
@@ -13,17 +13,17 @@ public class Solution {
     public ListNode detectCycle(ListNode head) {
         ListNode fast = head;
         ListNode slow = head;
-        while(fast != null && fast.next != null){
+        while(fast != null && fast.next!=null){
             fast = fast.next.next;
             slow = slow.next;
             if(fast == slow){
-                ListNode index1 = fast;
-                ListNode index2 = head;
-                while(index1 != index2){
-                    index1 = index1.next;
-                    index2 = index2.next;
+                ListNode indexA = head;
+                ListNode indexB = fast;
+                while(indexA != indexB){
+                    indexA = indexA.next;
+                    indexB = indexB.next;
                 }
-                return index1;
+                return indexA;
             }
         }
         return null;
