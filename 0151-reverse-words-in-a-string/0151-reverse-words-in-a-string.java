88c5bb0401371740931ee41dc21e3096be57661d
Chris Wang <100044659+ChrisWangCA@users.noreply.github.com>
2023-07-04 22:31:53 -0600

    Time: 6 ms (80.35%), Space: 43.7 MB (46.36%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -9,11 +9,12 @@ class Solution {
         }
         StringBuilder sb = new StringBuilder();
         for(int i=0;i<str.length;i++){
-            if(str[i].equals("")) continue;
-            if(i != str.length-1){
-                sb.append(str[i]).append(" ");
-            }else{
-                sb.append(str[i]);
+            if(!str[i].equals("")){
+                if(i == str.length-1){
+                    sb.append(str[i]);
+                }else{
+                    sb.append(str[i]).append(" ");
+                }
             }
         }
         return sb.toString();
