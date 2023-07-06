9fef9d57c1aedd645f607d51ca906843aeee949c
Chris Wang <100044659+ChrisWangCA@users.noreply.github.com>
2023-07-06 11:58:34 -0600

    Time: 122 ms (47.26%), Space: 44.2 MB (58.97%) - LeetHub

--- 0459-repeated-substring-pattern/0459-repeated-substring-pattern.java
+++ 0459-repeated-substring-pattern/0459-repeated-substring-pattern.java
@@ -1,5 +1,5 @@
 class Solution {
     public boolean repeatedSubstringPattern(String s) {
-        return (s + s).substring(1,2*s.length()-1).contains(s);
+        return (s+s).substring(1,2*s.length()-1).contains(s);
     }
 }
