fd004508d6a5b9035f85f912a75320f27ec24553
Chris Wang <100044659+ChrisWangCA@users.noreply.github.com>
2023-07-08 14:54:14 -0600

    Time: 18 ms (86.55%), Space: 44.1 MB (58.27%) - LeetHub

--- 0018-4sum/0018-4sum.java
+++ 0018-4sum/0018-4sum.java
@@ -6,7 +6,7 @@ class Solution {
             if(nums[i] > target && nums[i] > 0) return res;
             if(i > 0 && nums[i] == nums[i-1]) continue;
             for(int j=i+1;j<nums.length;j++){
-                if(j > i+1 && nums[j] == nums[j-1]) continue;
+                if(j>i+1 && nums[j] == nums[j-1]) continue;
                 int left = j+1;
                 int right = nums.length-1;
                 while(right > left){
@@ -17,10 +17,11 @@ class Solution {
                         left++;
                     }else{
                         res.add(Arrays.asList(nums[i],nums[j],nums[left],nums[right]));
-                            while(right>left && nums[right]==nums[right-1]) right--;
-                            while(right>left && nums[left] == nums[left+1]) left++;
-                            left++;
-                            right--;
+                        while(right>left && nums[right] == nums[right-1]) right--;
+                        while(right>left && nums[left] == nums[left+1]) left++;
+                        left++;
+                        right--;
+                        
                     }
                 }
             }
