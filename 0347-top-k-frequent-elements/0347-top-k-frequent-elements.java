4f609b29545c8299248b712058ced399b939c465
Chris Wang <100044659+ChrisWangCA@users.noreply.github.com>
2023-07-09 23:00:53 -0600

    Time: 14 ms (75.15%), Space: 47.5 MB (86.39%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -6,12 +6,12 @@ class Solution {
             map.put(i,map.getOrDefault(i,0)+1);
         }
         Set<Map.Entry<Integer,Integer>> sets = map.entrySet();
-        PriorityQueue<Map.Entry<Integer,Integer>> queue = new PriorityQueue<>((
-        o1,o2)->o2.getValue() - o1.getValue());
+        PriorityQueue<Map.Entry<Integer,Integer>> queue = new PriorityQueue<>(
+        (o1,o2)->o2.getValue() - o1.getValue());
         for(Map.Entry<Integer,Integer> set:sets){
             queue.offer(set);
         }
-        for(int i=k-1;i>=0;i--){
+        for(int i = k-1;i>=0;i--){
             res[i] = queue.poll().getKey();
         }
         return res;
