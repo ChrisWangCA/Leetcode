9e268e1279e1659d5d29e23b150151cb2ec0ad29
Chris Wang <100044659+ChrisWangCA@users.noreply.github.com>
2023-07-10 20:09:41 -0600

    Time: 47 ms (28.76%), Space: 59.9 MB (59.68%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.java
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.java
@@ -5,11 +5,11 @@ class Solution {
     public class MyQueue{
         void pop(int val){
             if(!queue.isEmpty() && val == queue.peek()){
-                queue.poll();
+                queue.pop();
             }
         }
         
-        void push(int val){
+        void add(int val){
             while(!queue.isEmpty() && val > queue.peekLast()){
                 queue.removeLast();
             }
@@ -22,18 +22,17 @@ class Solution {
     }
     
     public int[] maxSlidingWindow(int[] nums, int k) {
-        if(nums == null || nums.length < 2) return nums;
         int len = nums.length-k+1;
         int[] res = new int[len];
         int index = 0;
         MyQueue queue = new MyQueue();
         for(int i=0;i<k;i++){
-            queue.push(nums[i]);
+            queue.add(nums[i]);
         }
         res[index++] = queue.max();
         for(int i=k;i<nums.length;i++){
             queue.pop(nums[i-k]);
-            queue.push(nums[i]);
+            queue.add(nums[i]);
             res[index++] = queue.max();
         }
         return res;
