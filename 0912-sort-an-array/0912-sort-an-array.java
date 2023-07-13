2732c1c8850170d3212d0719eeade59b46848ce0
Chris Wang <100044659+ChrisWangCA@users.noreply.github.com>
2023-07-12 23:49:18 -0600

    Time: 43 ms (49.75%), Space: 54.6 MB (87.87%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.java
+++ 0912-sort-an-array/0912-sort-an-array.java
@@ -4,35 +4,35 @@ class Solution {
         return nums;
     }
     
-    public void mergeSort(int[] arr,int left,int right){
-        if(left == right) return;
-        int mid = left + (right-left)/2;
+    public void mergeSort(int[] arr,int left, int right){
+        if(left==right) return;
+        int mid = left + (right-left) / 2;
         mergeSort(arr,left,mid);
         mergeSort(arr,mid+1,right);
         merge(arr,left,mid+1,right);
     }
     
-    public void merge(int[] arr,int leftPtr,int rightPtr,int rightBound){
+    public void merge(int[] arr,int leftPtr, int rightPtr, int rightBound){
         int mid = rightPtr-1;
-        int[] temp = new int[rightBound-leftPtr+1];
         int i = leftPtr;
         int j = rightPtr;
+        int[] res = new int[rightBound-leftPtr+1];
         int k = 0;
-        while(i<=mid && j<=rightBound){
+        while(i<= mid && j<= rightBound){
             if(arr[i] <= arr[j]){
-                temp[k++] = arr[i++];
+                res[k++] = arr[i++];
             }else{
-                temp[k++] = arr[j++];
+                res[k++] = arr[j++];
             }
         }
-        while(i <= mid){
-            temp[k++] = arr[i++];
+        while(i<=mid){
+            res[k++]=arr[i++];
         }
-        while(j <= rightBound){
-            temp[k++] = arr[j++];
+        while(j<=rightBound){
+            res[k++]=arr[j++];
         }
-        for(int l=0;l<temp.length;l++){
-            arr[leftPtr+l] = temp[l];
+        for(int m=0;m<res.length;m++){
+            arr[leftPtr+m] = res[m];
         }
     }
 }
