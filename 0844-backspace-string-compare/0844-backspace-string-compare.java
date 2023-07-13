c02b40157b50ef75d8d5cfe93205e20966992769
Chris Wang <100044659+ChrisWangCA@users.noreply.github.com>
2023-07-12 23:18:39 -0600

    Time: 1 ms (83.15%), Space: 40.3 MB (97.05%) - LeetHub

--- 0844-backspace-string-compare/0844-backspace-string-compare.java
+++ 0844-backspace-string-compare/0844-backspace-string-compare.java
@@ -1,22 +1,22 @@
 class Solution {
     public boolean backspaceCompare(String s, String t) {
         char[] chs = s.toCharArray();
-        char[] cht = t.toCharArray();
         int slow = 0;
-        for(int i=0;i<chs.length;i++){
-            if(chs[i] == '#'){
-                slow = Math.max(0,slow-1);
+        for(int fast = 0;fast < chs.length; fast++){
+            if(chs[fast] != '#'){
+                chs[slow++] = chs[fast];
             }else{
-                chs[slow++] = chs[i];
+                slow = Math.max(0,slow-1);
             }
         }
         String s1 = new String(chs,0,slow);
+        char[] cht = t.toCharArray();
         slow = 0;
-        for(int i=0;i<cht.length;i++){
-            if(cht[i] == '#'){
-                slow = Math.max(0,slow-1);
+        for(int fast = 0; fast< cht.length;fast++){
+            if(cht[fast] != '#'){
+                cht[slow++] = cht[fast];
             }else{
-                cht[slow++] = cht[i];
+                slow = Math.max(0,slow-1);
             }
         }
         String s2 = new String(cht,0,slow);
