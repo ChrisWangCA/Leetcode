6465ebc06b86b186022b1b78c39bcc442694b746
Chris Wang <100044659+ChrisWangCA@users.noreply.github.com>
2023-07-17 23:27:43 -0600

    Time: 0 ms (100.00%), Space: 43.2 MB (58.67%) - LeetHub

--- 0559-maximum-depth-of-n-ary-tree/0559-maximum-depth-of-n-ary-tree.java
+++ 0559-maximum-depth-of-n-ary-tree/0559-maximum-depth-of-n-ary-tree.java
@@ -19,8 +19,8 @@ class Node {
 
 class Solution {
     public int maxDepth(Node root) {
-        if(root == null) return 0;
         int depth = 0;
+        if(root == null) return 0;
         if(root.children != null){
             for(Node child:root.children){
                 depth = Math.max(depth,maxDepth(child));
