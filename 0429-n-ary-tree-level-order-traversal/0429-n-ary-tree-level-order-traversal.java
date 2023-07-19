b8bc599613ce945b974a3745db9167902a67b09f
Chris Wang <100044659+ChrisWangCA@users.noreply.github.com>
2023-07-18 22:40:43 -0600

    Time: 4 ms (30.78%), Space: 43.8 MB (99.91%) - LeetHub

--- 0429-n-ary-tree-level-order-traversal/0429-n-ary-tree-level-order-traversal.java
+++ 0429-n-ary-tree-level-order-traversal/0429-n-ary-tree-level-order-traversal.java
@@ -26,15 +26,15 @@ class Solution {
         while(!queue.isEmpty()){
             int len = queue.size();
             List<Integer> list = new ArrayList<>();
-            while(len-->0){
+            while(len-- > 0){
                 Node temp = queue.poll();
-                list.add(temp.val);
                 List<Node> children = temp.children;
                 for(Node child:children){
                     if(child != null){
                         queue.offer(child);
                     }
                 }
+                list.add(temp.val);
             }
             res.add(list);
         }
