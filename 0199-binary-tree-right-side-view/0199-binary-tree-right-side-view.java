c56121efda0931a7eafa2ca73636ca17a981ab2a
Chris Wang <100044659+ChrisWangCA@users.noreply.github.com>
2023-07-18 22:38:11 -0600

    Time: 1 ms (67.19%), Space: 41.4 MB (42.63%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
@@ -16,20 +16,18 @@
 class Solution {
     public List<Integer> rightSideView(TreeNode root) {
         List<Integer> res = new ArrayList<>();
+        if(root == null) return res;
         Queue<TreeNode> queue = new LinkedList<>();
         queue.offer(root);
-        if(root == null) return res;
         while(!queue.isEmpty()){
             int len = queue.size();
             while(len-- > 0){
                 TreeNode temp = queue.poll();
-                if(temp.left != null){
-                    queue.offer(temp.left);
+                if(len == 0){
+                    res.add(temp.val);
                 }
-                if(temp.right != null){
-                    queue.offer(temp.right);
-                }
-                if(len == 0) res.add(temp.val);
+                if(temp.left != null) queue.offer(temp.left);
+                if(temp.right != null) queue.offer(temp.right);
             }
         }
         return res;
