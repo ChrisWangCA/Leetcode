c770657aa572701c9d13da2015b776eaa66bbb15
Chris Wang <100044659+ChrisWangCA@users.noreply.github.com>
2023-07-22 22:35:36 -0600

    Time: 0 ms (100.00%), Space: 40.2 MB (79.44%) - LeetHub

--- 0404-sum-of-left-leaves/0404-sum-of-left-leaves.java
+++ 0404-sum-of-left-leaves/0404-sum-of-left-leaves.java
@@ -15,8 +15,8 @@
  */
 class Solution {
     public int sumOfLeftLeaves(TreeNode root) {
-        if(root == null) return 0;
         int res = 0;
+        if(root == null) return 0;
         int left = sumOfLeftLeaves(root.left);
         int right = sumOfLeftLeaves(root.right);
         if(root.left != null && root.left.left == null && root.left.right == null){
