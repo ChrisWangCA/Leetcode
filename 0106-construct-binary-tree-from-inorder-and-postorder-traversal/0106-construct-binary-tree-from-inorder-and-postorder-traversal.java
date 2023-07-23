afc197e14eb234b0a8e082ef3b505184e2d8fb5d
Chris Wang <100044659+ChrisWangCA@users.noreply.github.com>
2023-07-22 20:50:55 -0600

    Time: 4 ms (30.91%), Space: 43.2 MB (83.54%) - LeetHub

--- 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.java
+++ 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.java
@@ -15,20 +15,18 @@
  */
 class Solution {
     public TreeNode buildTree(int[] inorder, int[] postorder) {
-        if(inorder.length==0 || postorder.length==0) return null;
+        if(inorder.length == 0 || postorder.length == 0) return null;
         return build(inorder,0,inorder.length,postorder,0,postorder.length);
     }
     
-    public TreeNode build(int[] inorder, int inStart, int inEnd,
-                         int[] postorder,int postStart,int postEnd){
+    public TreeNode build(int[] inorder,int inStart,int inEnd,
+                         int[] postorder, int postStart, int postEnd){
         if(postStart == postEnd) return null;
-        int rootValue = postorder[postEnd-1];
-        TreeNode root = new TreeNode(rootValue);
+        int rootVal = postorder[postEnd-1];
+        TreeNode root = new TreeNode(rootVal);
         int mid;
-        for(mid = inStart;mid<inEnd;mid++){
-            if(inorder[mid] == rootValue){
-                break;
-            }
+        for(mid = inStart;mid < inEnd;mid++){
+            if(inorder[mid] == rootVal) break;
         }
         int leftInStart = inStart;
         int leftInEnd = mid;
@@ -40,8 +38,10 @@ class Solution {
         int rightPostStart = leftPostEnd;
         int rightPostEnd = postEnd-1;
         
-        root.left = build(inorder,leftInStart, leftInEnd,postorder,leftPostStart,leftPostEnd);
-        root.right = build(inorder,rightInStart,rightInEnd,postorder,rightPostStart,rightPostEnd);
+        root.left = build(inorder,leftInStart,leftInEnd,
+                         postorder,leftPostStart,leftPostEnd);
+        root.right = build(inorder,rightInStart,rightInEnd,
+                          postorder,rightPostStart,rightPostEnd);
         return root;
     }
 }
