31eb1ef182edadaa9538de3dbf7ae531b5fb7702
Chris Wang <100044659+ChrisWangCA@users.noreply.github.com>
2023-07-22 21:31:37 -0600

    Time: 6 ms (96.02%), Space: 49.2 MB (99.44%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.java
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.java
@@ -1,19 +1,19 @@
 class Solution {
     public int totalFruit(int[] fruits) {
-        int maxFruit = 0;
-        int maxBasket = 0;
         int left = 0;
         int[] res = new int[fruits.length];
+        int maxFruit = 0;
+        int maxBasket = 0;
         for(int i=0;i<fruits.length;i++){
             int curFruit = fruits[i];
-            if(res[curFruit] == 0) maxBasket++;
-            res[curFruit]++;
+            if(res[curFruit] == 0){
+                maxBasket++;
+            }
+            res[curFruit] ++;
             while(maxBasket > 2){
                 int leftFruit = fruits[left];
                 res[leftFruit]--;
-                if(res[leftFruit] == 0){
-                    maxBasket--;
-                }
+                if(res[leftFruit] == 0) maxBasket--;
                 left++;
             }
             maxFruit = Math.max(maxFruit,i-left+1);
