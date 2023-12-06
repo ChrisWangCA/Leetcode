00ac1209def704c02dcbf61b7b0f630900a7a8d8
Chris Wang <100044659+ChrisWangCA@users.noreply.github.com>
2023-12-05 23:21:27 -0700

    Time: 0 ms (100.00%), Space: 41.2 MB (31.29%) - LeetHub

--- 0027-remove-element/0027-remove-element.java
+++ 0027-remove-element/0027-remove-element.java
@@ -1,8 +1,10 @@
 class Solution {
     public int removeElement(int[] nums, int val) {
         int slow = 0;
-        for(int fast = 0;fast < nums.length; fast++){
-            if(nums[fast] != val) nums[slow++] = nums[fast];
+        for(int fast = 0;fast < nums.length;fast++){
+            if(nums[fast] != val){
+                nums[slow++] = nums[fast];
+            }
         }
         return slow;
     }
