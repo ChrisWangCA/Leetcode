ffa2ad633cbb30fbe8291ded18566812ee960896
Chris Wang <100044659+ChrisWangCA@users.noreply.github.com>
2023-12-08 16:23:41 -0700

    Time: 1 ms (97.20%), Space: 44.2 MB (12.87%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -2,9 +2,7 @@ class Solution {
     public int removeDuplicates(int[] nums) {
         int slow = 0;
         for(int fast = 1;fast<nums.length;fast++){
-            if(nums[fast] != nums[slow]){
-                nums[++slow] = nums[fast];
-            }
+            if(nums[fast] != nums[slow]) nums[++slow] = nums[fast];
         }
         return slow+1;
     }
