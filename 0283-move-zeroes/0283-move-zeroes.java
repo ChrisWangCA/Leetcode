372190b787709daed9d444d027dbd0bd366a0f80
Chris Wang <100044659+ChrisWangCA@users.noreply.github.com>
2023-12-10 23:28:03 -0700

    Time: 2 ms (67.80%), Space: 44.9 MB (80.11%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,11 +1,11 @@
 class Solution {
     public void moveZeroes(int[] nums) {
         int slow = 0;
-        for(int fast = 0;fast < nums.length; fast++){
-            if(nums[fast] != 0){
-                int temp = nums[slow];
-                nums[slow] = nums[fast];
-                nums[fast] = temp;
+        for(int fast=0;fast<nums.length;fast++){
+            if(nums[fast]!=0){
+                int temp=nums[slow];
+                nums[slow]=nums[fast];
+                nums[fast]=temp;
                 slow++;
             }
         }
