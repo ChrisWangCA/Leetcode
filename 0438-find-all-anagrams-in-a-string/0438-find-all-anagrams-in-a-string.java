f7d939d9af800dbff27f574b773c2295dab9d47a
Chris Wang <100044659+ChrisWangCA@users.noreply.github.com>
2023-12-24 00:07:17 -0700

    Time: 10 ms (76.81%), Space: 44.6 MB (44.12%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
@@ -1,23 +1,33 @@
 class Solution {
     public List<Integer> findAnagrams(String s, String p) {
-            int step = 0;
-            char[] chp = p.toCharArray();
-            Arrays.sort(chp);
-            String s1 = new String(chp);
-            int len = chp.length;
-            int idx = step + len;
-            List<Integer> res = new ArrayList<>();
-            for(int i=0;i<=s.length()-len;i++){
-                String temp = s.substring(step,idx);
-                char[] c = temp.toCharArray();
-                Arrays.sort(c);
-                String s2 = new String(c);
-                step++;
-                idx++;
-                if(s1.equals(s2)){
-                    res.add(i);
-                }
+        if (p.length() > s.length()) {
+            return Collections.emptyList();
+        }
+
+        List<Integer> result = new ArrayList<>();
+        int[] pCount = new int[26];
+        int[] sCount = new int[26];
+
+        // 填充pCount和sCount的初始值
+        for (int i = 0; i < p.length(); i++) {
+            pCount[p.charAt(i) - 'a']++;
+            sCount[s.charAt(i) - 'a']++;
+        }
+
+        if (Arrays.equals(pCount, sCount)) {
+            result.add(0);
+        }
+
+        // 开始滑动窗口
+        for (int i = p.length(); i < s.length(); i++) {
+            sCount[s.charAt(i) - 'a']++;
+            sCount[s.charAt(i - p.length()) - 'a']--;
+
+            if (Arrays.equals(pCount, sCount)) {
+                result.add(i - p.length() + 1);
             }
-            return res;
+        }
+
+        return result;
     }
 }
