27e2e49ba03ad68cfab8f6be4de5d48f431b8534
Chris Wang <100044659+ChrisWangCA@users.noreply.github.com>
2024-01-05 22:49:31 -0700

    Time: 3 ms (84.05%), Space: 43.3 MB (63.21%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,14 +1,15 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
+        if(s.length() != t.length()) return false;
         int[] res = new int[26];
-        for(char chs:s.toCharArray()){
-            res[chs-'a']+=1;
+        for(char c:s.toCharArray()){
+            res[c-'a']+=1;
         }
-        for(char cht:t.toCharArray()){
-            res[cht-'a']-=1;
+        for(char c:t.toCharArray()){
+            res[c-'a']-=1;
         }
         for(int i:res){
-            if(i!=0) return false;
+            if(i != 0) return false;
         }
         return true;
     }
