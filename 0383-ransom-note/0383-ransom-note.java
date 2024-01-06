8386bc112f57ca1e87dd169d78394d8697ca4c4e
Chris Wang <100044659+ChrisWangCA@users.noreply.github.com>
2024-01-05 22:51:17 -0700

    Time: 1 ms (99.24%), Space: 44.4 MB (36.91%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.java
+++ 0383-ransom-note/0383-ransom-note.java
@@ -1,12 +1,13 @@
 class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
+        if(ransomNote.length() > magazine.length()) return false;
         int[] res = new int[26];
-        for(char chs:magazine.toCharArray()){
-            res[chs-'a'] += 1;
+        for(char c:magazine.toCharArray()){
+            res[c-'a'] += 1;
         }
-        for(char cht:ransomNote.toCharArray()){
-            res[cht-'a'] -= 1;
-            if(res[cht-'a'] < 0) return false;
+        for(char c:ransomNote.toCharArray()){
+            res[c-'a'] -=1;
+            if(res[c-'a'] < 0) return false;
         }
         return true;
     }
