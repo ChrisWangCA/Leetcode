a0e1bd53491a9b78a8ca67ea2a1f2b1ae214bc45
Chris Wang <100044659+ChrisWangCA@users.noreply.github.com>
2024-01-07 17:25:26 -0700

    Time: 12 ms (83.41%), Space: 44 MB (66.67%) - LeetHub

--- 0018-4sum/0018-4sum.java
+++ 0018-4sum/0018-4sum.java
@@ -8,20 +8,20 @@ class Solution {
             if(i > 0 && nums[i] == nums[i-1]) continue;
             for(int j=i+1;j<nums.length;j++){
                 if(j > i+1 && nums[j] == nums[j-1]) continue;
-                int left = j+1;
-                int right = nums.length-1;
-                while(left < right){
-                    int sum = nums[i]+nums[j]+nums[left]+nums[right];
-                    if(sum > target){
-                        right--;
-                    }else if(sum < target){
-                        left++;
+                int l = j+1;
+                int r = nums.length-1;
+                while(l < r){
+                    int sum = nums[i]+nums[j]+nums[l]+nums[r];
+                    if(sum>target){
+                        r--;
+                    }else if(sum<target){
+                        l++;
                     }else{
-                        res.add(Arrays.asList(nums[i],nums[j],nums[left],nums[right]));
-                        while(left<right && nums[left] == nums[left+1]) left++;
-                        while(left<right && nums[right] == nums[right-1]) right--;
-                        left++;
-                        right--;
+                        res.add(Arrays.asList(nums[i],nums[j],nums[l],nums[r]));
+                        while(l<r && nums[l] == nums[l+1]) l++;
+                        while(l<r && nums[r] == nums[r-1]) r--;
+                        l++;
+                        r--;
                     }
                 }
             }
