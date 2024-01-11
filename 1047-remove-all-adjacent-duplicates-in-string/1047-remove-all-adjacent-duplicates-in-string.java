b7926a608e84dcb72556473667fa24989531f886
Chris Wang <100044659+ChrisWangCA@users.noreply.github.com>
2024-01-10 20:49:16 -0700

    Time: 4 ms (99.60%), Space: 44.5 MB (78.24%) - LeetHub

--- 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.java
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.java
@@ -1,15 +1,15 @@
 class Solution {
     public String removeDuplicates(String s) {
-        int left = 0;
+        int slow = 0;
         char[] chs = s.toCharArray();
         for(int i=0;i<chs.length;i++){
-            chs[left] = chs[i];
-            if(left > 0 && chs[left] == chs[left-1]){
-                left--;
+            chs[slow] = chs[i];
+            if(slow > 0 && chs[slow] == chs[slow-1]){
+                slow--;
             }else{
-                left++;
+                slow++;
             }
         }
-        return new String(chs,0,left);
+        return new String(chs,0,slow);
     }
 }
