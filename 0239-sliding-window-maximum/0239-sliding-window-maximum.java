b8c76d367cb01633393d9f1421f1bfb85fe1d005
Chris Wang <100044659+ChrisWangCA@users.noreply.github.com>
2024-01-11 23:47:19 -0700

    Time: 36 ms (37.81%), Space: 58.4 MB (79.25%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.java
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.java
@@ -1,7 +1,7 @@
 class Solution {
     Deque<Integer> queue = new ArrayDeque<>();
     class MyQueue{
-        void push(int val){
+        void add(int val){
             while(!queue.isEmpty() && val > queue.peekLast()){
                 queue.pollLast();
             }
@@ -9,7 +9,7 @@ class Solution {
         }
         void pop(int val){
             if(!queue.isEmpty() && val == queue.peekFirst()){
-                queue.pollFirst();
+                queue.pop();
             }
         }
         int max(){
@@ -22,12 +22,12 @@ class Solution {
         int index = 0;
         MyQueue queue = new MyQueue();
         for(int i=0;i<k;i++){
-            queue.push(nums[i]);
+            queue.add(nums[i]);
         }
         res[index++] = queue.max();
         for(int i=k;i<nums.length;i++){
             queue.pop(nums[i-k]);
-            queue.push(nums[i]);
+            queue.add(nums[i]);
             res[index++] = queue.max();
         }
         return res;
