d24e84f794e49b0b1d50aec223f72008405d5af2
Chris Wang <100044659+ChrisWangCA@users.noreply.github.com>
2024-01-11 23:36:28 -0700

    Time: 4 ms (74.72%), Space: 63.2 MB (27.68%) - LeetHub

--- 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.java
+++ 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.java
@@ -20,6 +20,6 @@ class Solution {
         int right = minDepth(root.right);
         if(left == 0) return right+1;
         if(right == 0) return left+1;
-        return Math.min(left,right) + 1;
+        return Math.min(left,right)+1;
     }
 }
