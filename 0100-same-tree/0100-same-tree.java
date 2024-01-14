0330a5ecfcf710da1132b1e822ded1e3022333af
Chris Wang <100044659+ChrisWangCA@users.noreply.github.com>
2024-01-13 23:43:29 -0700

    Time: 0 ms (100.00%), Space: 41 MB (25.80%) - LeetHub

--- 0100-same-tree/0100-same-tree.java
+++ 0100-same-tree/0100-same-tree.java
@@ -15,8 +15,11 @@
  */
 class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
+        return isSame(p,q);
+    }
+    public boolean isSame(TreeNode p, TreeNode q){
         if(p == null && q == null) return true;
         if(p == null || q == null || p.val != q.val) return false;
-        return isSameTree(p.left,q.left) && isSameTree(p.right,q.right);
+        return isSame(p.left,q.left) && isSame(p.right,q.right);
     }
 }
