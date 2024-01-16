e1d108f544dff8693d080e0ecc93aeccdfee6896
Chris Wang <100044659+ChrisWangCA@users.noreply.github.com>
2024-01-15 20:24:09 -0700

    Time: 4 ms (19.39%), Space: 44.3 MB (30.43%) - LeetHub

--- 0572-subtree-of-another-tree/0572-subtree-of-another-tree.java
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.java
@@ -17,8 +17,9 @@ class Solution {
     public boolean isSubtree(TreeNode root, TreeNode subRoot) {
         if(root == null) return subRoot == null;
         if(isSame(root,subRoot)) return true;
-        return isSubtree(root.left,subRoot) || isSubtree(root.right,subRoot);
+        return isSubtree(root.left,subRoot) || isSubtree(root.right, subRoot);
     }
+    
     public boolean isSame(TreeNode root, TreeNode subRoot){
         if(root == null && subRoot == null) return true;
         if(root == null || subRoot == null || root.val != subRoot.val) return false;
