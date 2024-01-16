995029cf3758274659761c876171aa29d6dce21c
Chris Wang <100044659+ChrisWangCA@users.noreply.github.com>
2024-01-16 15:31:10 -0700

    Time: 2 ms (97.03%), Space: 43.5 MB (60.32%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,12 +1,11 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        if(s.length() != t.length()) return false;
         int[] res = new int[26];
         for(char c:s.toCharArray()){
-            res[c-'a']+=1;
+            res[c-'a'] += 1;
         }
         for(char c:t.toCharArray()){
-            res[c-'a']-=1;
+            res[c-'a'] -= 1;
         }
         for(int i:res){
             if(i != 0) return false;
