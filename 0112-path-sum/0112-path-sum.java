950ce3346ca9f5fcabf08d372c4cfe105462acb6
Chris Wang <100044659+ChrisWangCA@users.noreply.github.com>
2024-01-16 14:51:21 -0700

    Time: 0 ms (100.00%), Space: 42.9 MB (88.97%) - LeetHub

--- 0112-path-sum/0112-path-sum.java
+++ 0112-path-sum/0112-path-sum.java
@@ -19,7 +19,7 @@ class Solution {
         if(root.left == null && root.right == null){
             return targetSum - root.val == 0;
         }
-        return hasPathSum(root.left,targetSum - root.val) ||
-            hasPathSum(root.right,targetSum - root.val);
+        return hasPathSum(root.left,targetSum-root.val) ||
+            hasPathSum(root.right,targetSum-root.val);
     }
 }
