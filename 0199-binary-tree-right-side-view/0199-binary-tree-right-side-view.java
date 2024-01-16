92a230c2d3f7de56a8016064a49aca142aa5beec
Chris Wang <100044659+ChrisWangCA@users.noreply.github.com>
2024-01-16 15:10:14 -0700

    Time: 1 ms (71.38%), Space: 42.1 MB (25.00%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
@@ -20,12 +20,12 @@ class Solution {
         Queue<TreeNode> queue = new LinkedList<>();
         queue.offer(root);
         while(!queue.isEmpty()){
-            int size = queue.size();
-            while(size-- > 0){
+            int len = queue.size();
+            while(len-- > 0){
                 TreeNode temp = queue.poll();
-                if(temp.left!=null) queue.offer(temp.left);
-                if(temp.right!=null) queue.offer(temp.right);
-                if(size == 0) res.add(temp.val);
+                if(temp.left != null) queue.offer(temp.left);
+                if(temp.right != null) queue.offer(temp.right);
+                if(len == 0) res.add(temp.val);
             }
         }
         return res;
