ce8b50af644270b40c9de93e840a9e7f5a26f615
Chris Wang <100044659+ChrisWangCA@users.noreply.github.com>
2024-01-18 15:18:15 -0700

    Time: 0 ms (100.00%), Space: 45.2 MB (30.80%) - LeetHub

--- 0700-search-in-a-binary-search-tree/0700-search-in-a-binary-search-tree.java
+++ 0700-search-in-a-binary-search-tree/0700-search-in-a-binary-search-tree.java
@@ -18,10 +18,8 @@ class Solution {
         if(root == null || root.val == val) return root;
         if(root.val > val){
             return searchBST(root.left,val);
-        }
-        if(root.val < val){
+        }else{
             return searchBST(root.right,val);
         }
-        return null;
     }
 }
