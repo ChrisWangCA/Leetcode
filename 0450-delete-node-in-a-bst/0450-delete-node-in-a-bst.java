8c875dc7992aed48261c98b19d70477d2a071fb4
Chris Wang <100044659+ChrisWangCA@users.noreply.github.com>
2024-01-19 15:13:28 -0700

    Time: 0 ms (100.00%), Space: 46.2 MB (17.57%) - LeetHub

--- 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.java
+++ 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.java
@@ -15,7 +15,7 @@
  */
 class Solution {
     public TreeNode deleteNode(TreeNode root, int key) {
-        if(root == null) return root;
+        if(root == null) return null;
         if(root.val == key){
             if(root.left == null && root.right == null){
                 return null;
@@ -32,8 +32,12 @@ class Solution {
                 return root.right;
             }
         }
-        if(root.val < key) root.right = deleteNode(root.right,key);
-        if(root.val > key) root.left = deleteNode(root.left,key);
+        if(root.val > key){
+            root.left = deleteNode(root.left,key);
+        }
+        if(root.val < key){
+            root.right = deleteNode(root.right,key);
+        }
         return root;
     }
 }
