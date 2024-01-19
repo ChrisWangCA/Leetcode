104dffad0c2de57160a0a1fc1ddcaae50b25aca7
Chris Wang <100044659+ChrisWangCA@users.noreply.github.com>
2024-01-19 15:07:47 -0700

    Time: 0 ms (100.00%), Space: 44.7 MB (11.18%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.java
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.java
@@ -16,12 +16,14 @@
 class Solution {
     public boolean isBalanced(TreeNode root) {
         if(root == null) return true;
-        return Math.abs(ABS(root.left)-ABS(root.right)) <= 1 && isBalanced(root.left)
-            && isBalanced(root.right);
+        return Math.abs(getHeight(root.left) - getHeight(root.right)) <= 1 &&
+            isBalanced(root.left) && isBalanced(root.right);
     }
     
-    public int ABS(TreeNode root){
+    public int getHeight(TreeNode root){
         if(root == null) return 0;
-        return Math.max(ABS(root.left),ABS(root.right))+1;
+        int left = getHeight(root.left);
+        int right = getHeight(root.right);
+        return Math.max(left,right)+1;
     }
 }
