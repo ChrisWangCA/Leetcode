ad9d12b7303d46825537252e35ee1b9cb518a661
Chris Wang <100044659+ChrisWangCA@users.noreply.github.com>
2024-01-20 16:27:24 -0700

    Time: 12 ms (83.88%), Space: 44 MB (65.43%) - LeetHub

--- 0018-4sum/0018-4sum.java
+++ 0018-4sum/0018-4sum.java
@@ -1,27 +1,27 @@
 class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
         List<List<Integer>> res = new ArrayList<>();
-        if(nums == null || nums.length < 3) return res;
+        if(nums.length < 3 || nums == null) return res;
         Arrays.sort(nums);
         for(int i=0;i<nums.length;i++){
             if(nums[i] > 0 && nums[i] > target) return res;
             if(i > 0 && nums[i] == nums[i-1]) continue;
             for(int j=i+1;j<nums.length;j++){
                 if(j > i+1 && nums[j] == nums[j-1]) continue;
-                int left = j+1;
-                int right = nums.length-1;
-                while(left < right){
-                    int sum = nums[i] + nums[j] + nums[left] + nums[right];
+                int l = j+1;
+                int r = nums.length-1;
+                while(l < r){
+                    int sum = nums[i]+nums[j]+nums[l]+nums[r];
                     if(sum > target){
-                        right--;
+                        r--;
                     }else if(sum < target){
-                        left++;
+                        l++;
                     }else{
-                        res.add(Arrays.asList(nums[i],nums[j],nums[left],nums[right]));
-                        while(left<right && nums[left] == nums[left+1]) left++;
-                        while(left<right && nums[right] == nums[right-1]) right--;
-                        left++;
-                        right--;
+                        res.add(Arrays.asList(nums[i],nums[j],nums[l],nums[r]));
+                        while(l<r&&nums[l]==nums[l+1])l++;
+                        while(l<r&&nums[r]==nums[r-1])r--;
+                        l++;
+                        r--;
                     }
                 }
             }
