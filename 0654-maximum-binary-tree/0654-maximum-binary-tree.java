37c984bb28ee228d2d5fa6d0ce660bfa22be3c32
Chris Wang <100044659+ChrisWangCA@users.noreply.github.com>
2024-01-20 23:16:22 -0700

    Time: 2 ms (65.82%), Space: 45 MB (17.35%) - LeetHub

--- 0654-maximum-binary-tree/0654-maximum-binary-tree.java
+++ 0654-maximum-binary-tree/0654-maximum-binary-tree.java
@@ -17,14 +17,13 @@ class Solution {
     public TreeNode constructMaximumBinaryTree(int[] nums) {
         return construct(nums,0,nums.length);
     }
-    
-    public TreeNode construct(int[] nums,int left,int right){
-        if(right-left < 1) return null;
-        if(right-left == 1) return new TreeNode(nums[left]);
+    public TreeNode construct(int[] nums,int left, int right){
+        if(right - left < 1) return null;
+        if(right - left == 0) return new TreeNode(nums[left]);
         int maxValue = nums[left];
         int maxIndex = left;
         for(int i=left+1;i<right;i++){
-            if(nums[i] > maxValue){
+            if(maxValue < nums[i]){
                 maxValue = nums[i];
                 maxIndex = i;
             }
