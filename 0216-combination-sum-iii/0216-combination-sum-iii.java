bb124a0afa9a390983a5964ad682af1ade7e46cd
Chris Wang <100044659+ChrisWangCA@users.noreply.github.com>
2024-01-21 21:43:35 -0700

    Time: 0 ms (100.00%), Space: 41 MB (25.88%) - LeetHub

---
+++ 0216-combination-sum-iii/0216-combination-sum-iii.java
@@ -0,0 +1,24 @@
+class Solution {
+    List<List<Integer>> res = new ArrayList<>();
+    List<Integer> path = new ArrayList<>();
+    public List<List<Integer>> combinationSum3(int k, int n) {
+        backtracing(k,n,0,1);
+        return res;
+    }
+    
+    public void backtracing(int k,int target,int sum, int startIndex){
+        if(path.size() == k){
+            if(target == sum){
+                res.add(new ArrayList<>(path));
+            }
+            return;
+        }
+        for(int i=startIndex;i<=9;i++){
+            path.add(i);
+            sum += i;
+            backtracing(k,target,sum,i+1);
+            sum -= i;
+            path.remove(path.size()-1);
+        }
+    }
+}
