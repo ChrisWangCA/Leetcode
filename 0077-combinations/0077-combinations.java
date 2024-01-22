508aec71aef7431508832d570692e6471e34f83e
Chris Wang <100044659+ChrisWangCA@users.noreply.github.com>
2024-01-22 15:29:49 -0700

    Time: 19 ms (52.51%), Space: 94.4 MB (44.45%) - LeetHub

--- 0077-combinations/0077-combinations.java
+++ 0077-combinations/0077-combinations.java
@@ -1,20 +1,20 @@
 class Solution {
     List<List<Integer>> res = new ArrayList<>();
-    List<Integer> list = new ArrayList<>();
+    List<Integer> path = new ArrayList<>();
     public List<List<Integer>> combine(int n, int k) {
         backtracing(n,k,1);
         return res;
     }
     
     public void backtracing(int n,int k,int startIndex){
-        if(list.size() == k){
-            res.add(new ArrayList<>(list));
+        if(path.size() == k){
+            res.add(new ArrayList<>(path));
             return;
         }
         for(int i=startIndex;i<=n;i++){
-            list.add(i);
+            path.add(i);
             backtracing(n,k,i+1);
-            list.remove(list.size()-1);
+            path.remove(path.size()-1);
         }
     }
 }
