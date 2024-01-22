abab2e32d6cce5401d252473342d937670e0f7a2
Chris Wang <100044659+ChrisWangCA@users.noreply.github.com>
2024-01-22 15:47:53 -0700

    Time: 0 ms (100.00%), Space: 42 MB (43.29%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -1,23 +1,15 @@
 class Solution {
-    StringBuilder sb = new StringBuilder();
     List<String> res = new ArrayList<>();
+    StringBuilder sb = new StringBuilder();
     public List<String> letterCombinations(String digits) {
+        String[] strs = {"","",
+                        "abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
         if(digits == null || digits.length() == 0) return res;
-        String[] strs = {"",
-                        "",
-                        "abc",
-                        "def",
-                        "ghi",
-                        "jkl",
-                        "mno",
-                        "pqrs",
-                        "tuv",
-                        "wxyz"};
         backtracing(digits,strs,0);
         return res;
     }
     
-    public void backtracing(String digits, String[] strs, int nums){
+    public void backtracing(String digits,String[] strs,int nums){
         if(nums == digits.length()){
             res.add(sb.toString());
             return;
