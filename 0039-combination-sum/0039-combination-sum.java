ff72cc37d5a870df75190c7fffedc06242211382
Chris Wang <100044659+ChrisWangCA@users.noreply.github.com>
2024-01-22 15:55:28 -0700

    Time: 2 ms (82.92%), Space: 44.5 MB (37.79%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -4,20 +4,17 @@ class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         backtracing(candidates,target,0,0);
         return res;
-        
     }
     
-    public void backtracing(int[] candidates, int target,int sum,int startIndex){
+    public void backtracing(int[] candidates,int target,int sum, int startIndex){
         if(sum > target) return;
         if(sum == target){
             res.add(new ArrayList<>(path));
             return;
         }
- 
-        
         for(int i=startIndex;i<candidates.length;i++){
-            path.add(candidates[i]);
             sum += candidates[i];
+            path.add(candidates[i]);
             backtracing(candidates,target,sum,i);
             sum -= candidates[i];
             path.remove(path.size()-1);
