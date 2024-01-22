081e61353bfd8e618c92c17e4a1654a91629d4a6
Chris Wang <100044659+ChrisWangCA@users.noreply.github.com>
2024-01-21 17:58:53 -0700

    Time: 1 ms (98.67%), Space: 41.8 MB (6.39%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,5 +1,6 @@
 class Solution {
     public boolean isValid(String s) {
+        if(s.length() % 2 != 0) return false;
         Stack<Character> stack = new Stack<>();
         for(char c:s.toCharArray()){
             if(c == '('){
@@ -8,7 +9,7 @@ class Solution {
                 stack.push(']');
             }else if(c == '{'){
                 stack.push('}');
-            }else if(stack.isEmpty() || c != stack.peek()){
+            }else if(stack.isEmpty() || stack.peek() != c){
                 return false;
             }else{
                 stack.pop();
