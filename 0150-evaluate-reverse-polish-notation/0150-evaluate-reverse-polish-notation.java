db9dbaa1b95089daf8a85757d0fc0f9451166791
Chris Wang <100044659+ChrisWangCA@users.noreply.github.com>
2024-01-22 23:48:27 -0700

    Time: 6 ms (78.22%), Space: 44.4 MB (54.93%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
@@ -1,19 +1,19 @@
 class Solution {
     public int evalRPN(String[] tokens) {
         Stack<Integer> stack = new Stack<>();
-        for(String str:tokens){
-            if(!str.equals("+") &&!str.equals("-") &&!str.equals("*") &&!str.equals("/")){
-                stack.push(Integer.valueOf(str));
+        for(String s:tokens){
+            if(!s.equals("+") &&!s.equals("-") &&!s.equals("*") &&!s.equals("/")){
+                stack.push(Integer.valueOf(s));
             }else{
                 int n1 = stack.pop();
                 int n2 = stack.pop();
-                if(str.equals("+")){
-                    stack.push(n1+n2);
-                }else if(str.equals("-")){
+                if(s.equals("+")){
+                    stack.push(n2+n1);
+                }else if(s.equals("-")){
                     stack.push(n2-n1);
-                }else if(str.equals("*")){
+                }else if(s.equals("*")){
                     stack.push(n2*n1);
-                }else if(str.equals("/")){
+                }else if(s.equals("/")){
                     stack.push(n2/n1);
                 }
             }
