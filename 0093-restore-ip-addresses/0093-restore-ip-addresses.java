fb5d5363ae0a0056f299a46288aedda7d77f1bdc
Chris Wang <100044659+ChrisWangCA@users.noreply.github.com>
2024-01-23 22:54:35 -0700

    Time: 3 ms (64.02%), Space: 41.3 MB (95.28%) - LeetHub

---
+++ 0093-restore-ip-addresses/0093-restore-ip-addresses.java
@@ -0,0 +1,40 @@
+class Solution {
+    List<String> res = new ArrayList<>();
+    public List<String> restoreIpAddresses(String s) {
+        StringBuilder sb = new StringBuilder(s);
+        backtracing(sb,0,0);
+        return res;
+    }
+    
+    public void backtracing(StringBuilder s, int startIndex, int dotPoint){
+        if(dotPoint == 3){
+            if(isValid(s,startIndex,s.length()-1)){
+                res.add(s.toString());
+                return;
+            }
+        }
+        for(int i=startIndex;i<s.length();i++){
+            if(isValid(s,startIndex,i)){
+                s.insert(i+1,".");
+                backtracing(s,i+2,dotPoint+1);
+                s.deleteCharAt(i+1);
+            }else{
+                break;
+            }
+        }
+    }
+    
+    public boolean isValid(StringBuilder s,int start,int end){
+        if(start > end) return false;
+        if(s.charAt(start) == '0' && start != end) return false;
+        int num = 0;
+        for(int i=start;i<=end;i++){
+            int digit = s.charAt(i)-'0';
+            num = num * 10 + digit;
+            if(num > 255){
+                return false;
+            }
+        }
+        return true;
+    }
+}
