a43fc97cc8bb22f2e7062d833c360eb09718aaba
Chris Wang <100044659+ChrisWangCA@users.noreply.github.com>
2024-01-24 21:32:43 -0700

    Time: 5 ms (92.55%), Space: 51 MB (54.52%) - LeetHub

---
+++ 0491-non-decreasing-subsequences/0491-non-decreasing-subsequences.java
@@ -0,0 +1,24 @@
+class Solution {
+    List<List<Integer>> res = new ArrayList<>();
+    List<Integer> path = new ArrayList<>();
+    public List<List<Integer>> findSubsequences(int[] nums) {
+        backtracing(nums,0);
+        return res;
+    }
+    
+    public void backtracing(int[] nums,int startIndex){
+        if(path.size() >= 2){
+            res.add(new ArrayList<>(path));
+        }
+        Set<Integer> set = new HashSet<>();
+
+        for(int i=startIndex;i<nums.length;i++){
+            if(!path.isEmpty() && path.get(path.size()-1) > nums[i] ||
+              set.contains(nums[i])) continue;
+            path.add(nums[i]);
+            set.add(nums[i]);
+            backtracing(nums,i+1);
+            path.remove(path.size()-1);
+        }
+    }
+}
