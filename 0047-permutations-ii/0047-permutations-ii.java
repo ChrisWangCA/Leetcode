5d8337d6c56913a2bda7e895ea75ed2a1f098d33
Chris Wang <100044659+ChrisWangCA@users.noreply.github.com>
2024-01-24 23:24:54 -0700

    Time: 2 ms (84.37%), Space: 44.7 MB (60.15%) - LeetHub

---
+++ 0047-permutations-ii/0047-permutations-ii.java
@@ -0,0 +1,28 @@
+class Solution {
+    List<List<Integer>> res = new ArrayList<>();
+    List<Integer> path = new ArrayList<>();
+    
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        Arrays.sort(nums);
+        boolean[] used = new boolean[nums.length];
+        backtracing(nums,used);
+        return res;
+    }
+    
+    public void backtracing(int[] nums,boolean[] used){
+        if(nums.length == path.size()){
+            res.add(new ArrayList<>(path));
+            return;
+        }
+        
+        for(int i=0;i<nums.length;i++){
+            if(i > 0 && nums[i] == nums[i-1] && used[i-1] == false) continue;
+            if(used[i] == true) continue;
+            path.add(nums[i]);
+            used[i] = true;
+            backtracing(nums,used);
+            path.remove(path.size()-1);
+            used[i] = false;
+        }
+    }
+}
