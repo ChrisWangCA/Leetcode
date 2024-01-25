064572403ab3c32d99168cf6b9260a8cb1ace226
Chris Wang <100044659+ChrisWangCA@users.noreply.github.com>
2024-01-25 16:15:56 -0700

    Time: 7 ms (71.03%), Space: 51.3 MB (37.69%) - LeetHub

--- 0491-non-decreasing-subsequences/0491-non-decreasing-subsequences.java
+++ 0491-non-decreasing-subsequences/0491-non-decreasing-subsequences.java
@@ -11,9 +11,8 @@ class Solution {
             res.add(new ArrayList<>(path));
         }
         Set<Integer> set = new HashSet<>();
-
         for(int i=startIndex;i<nums.length;i++){
-            if(!path.isEmpty() && path.get(path.size()-1) > nums[i] ||
+            if(!path.isEmpty() && nums[i] < path.get(path.size()-1) ||
               set.contains(nums[i])) continue;
             path.add(nums[i]);
             set.add(nums[i]);
