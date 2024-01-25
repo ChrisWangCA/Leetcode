19e9ed86653e49927329f80876598beb19179d01
Chris Wang <100044659+ChrisWangCA@users.noreply.github.com>
2024-01-24 17:16:00 -0700

    Time: 7 ms (86.32%), Space: 57.2 MB (15.42%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.java
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.java
@@ -1,18 +1,19 @@
 class Solution {
     List<List<String>> res = new ArrayList<>();
     List<String> path = new ArrayList<>();
+    
     public List<List<String>> partition(String s) {
         backtracing(s,0);
         return res;
     }
     
-    public void backtracing(String s, int startIndex){
+    public void backtracing(String s,int startIndex){
         if(startIndex == s.length()){
             res.add(new ArrayList<>(path));
             return;
         }
         for(int i=startIndex;i<s.length();i++){
-            if(isPalindrome(s,startIndex,i)){
+            if(isValid(s,startIndex,i)){
                 String str = s.substring(startIndex,i+1);
                 path.add(str);
             }else{
@@ -23,11 +24,9 @@ class Solution {
         }
     }
     
-    public boolean isPalindrome(String s,int start, int end){
-        for(int i=start;i<end;i++,end--){
-            if(s.charAt(i) != s.charAt(end)){
-                return false;
-            }
+    public boolean isValid(String s,int start,int end){
+        for(int i=start;i<=end;i++,end--){
+            if(s.charAt(i) != s.charAt(end)) return false;
         }
         return true;
     }
