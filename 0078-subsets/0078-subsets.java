ffec56702fca6b7822afdefa167ec7f7aa5d84de
Chris Wang <100044659+ChrisWangCA@users.noreply.github.com>
2024-01-26 15:41:13 -0700

    Time: 1 ms (71.50%), Space: 42.7 MB (25.95%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -1,17 +1,13 @@
 class Solution {
     List<List<Integer>> res = new ArrayList<>();
     List<Integer> path = new ArrayList<>();
-    
     public List<List<Integer>> subsets(int[] nums) {
         backtracing(nums,0);
         return res;
     }
-    
     public void backtracing(int[] nums,int startIndex){
         res.add(new ArrayList<>(path));
-        if(startIndex == nums.length){
-            return;
-        }
+        if(startIndex == nums.length) return;
         for(int i=startIndex;i<nums.length;i++){
             path.add(nums[i]);
             backtracing(nums,i+1);
