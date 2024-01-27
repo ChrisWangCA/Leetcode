06f2724423a6363db3b89f9fac3011c8b87150a8
Chris Wang <100044659+ChrisWangCA@users.noreply.github.com>
2024-01-27 15:21:44 -0700

    Time: 1 ms (99.88%), Space: 43.2 MB (93.33%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.java
+++ 0090-subsets-ii/0090-subsets-ii.java
@@ -1,5 +1,4 @@
 class Solution {
-    
     List<List<Integer>> res = new ArrayList<>();
     List<Integer> path = new ArrayList<>();
     public List<List<Integer>> subsetsWithDup(int[] nums) {
@@ -10,9 +9,7 @@ class Solution {
     
     public void backtracing(int[] nums,int startIndex){
         res.add(new ArrayList<>(path));
-        if(startIndex == nums.length){
-            return;
-        }
+        if(startIndex == nums.length) return;
         for(int i=startIndex;i<nums.length;i++){
             if(i > startIndex && nums[i] == nums[i-1]) continue;
             path.add(nums[i]);
