ffb338feebb87f7f1f30038be62792f5b26063d5
Chris Wang <100044659+ChrisWangCA@users.noreply.github.com>
2024-01-27 23:43:25 -0700

    Time: 0 ms (100.00%), Space: 43.2 MB (53.75%) - LeetHub

--- 0108-convert-sorted-array-to-binary-search-tree/0108-convert-sorted-array-to-binary-search-tree.java
+++ 0108-convert-sorted-array-to-binary-search-tree/0108-convert-sorted-array-to-binary-search-tree.java
@@ -18,12 +18,13 @@ class Solution {
         return sorted(nums,0,nums.length-1);
     }
     
-    public TreeNode sorted(int[] res,int left,int right){
+    public TreeNode sorted(int[] nums,int left,int right){
         if(left > right) return null;
+        if(left == right) return new TreeNode(nums[left]);
         int mid = left + ((right-left)/2);
-        TreeNode root = new TreeNode(res[mid]);
-        root.left = sorted(res,left,mid-1);
-        root.right = sorted(res,mid+1,right);
+        TreeNode root = new TreeNode(nums[mid]);
+        root.left = sorted(nums,left,mid-1);
+        root.right = sorted(nums,mid+1,right);
         return root;
     }
 }
