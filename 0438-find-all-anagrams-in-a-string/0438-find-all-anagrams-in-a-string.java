441ba0680beccb2730310481ed13534ac2234c60
Chris Wang <100044659+ChrisWangCA@users.noreply.github.com>
2024-01-28 23:48:04 -0700

    Time: 11 ms (75.42%), Space: 44.5 MB (79.37%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
@@ -1,10 +1,10 @@
 class Solution {
     public List<Integer> findAnagrams(String s, String p) {
         List<Integer> res = new ArrayList<>();
-        int left = 0;
-        if(p.length() > s.length()) return res;
         int[] s1 = new int[26];
         int[] p1 = new int[26];
+        if(p.length() > s.length()) return res;
+        int left = 0;
         for(int i=0;i<p.length();i++){
             s1[s.charAt(i)-'a'] += 1;
             p1[p.charAt(i)-'a'] += 1;
@@ -13,8 +13,8 @@ class Solution {
             res.add(0);
         }
         for(int i=p.length();i<s.length();i++){
-            s1[s.charAt(i)-'a'] += 1;
             s1[s.charAt(i-p.length())-'a'] -= 1;
+            s1[s.charAt(i)-'a'] += 1;
             if(Arrays.equals(s1,p1)){
                 res.add(i-p.length()+1);
             }
