0ddd6cccc030c0a354569377bf0507b9c655e1c0
Chris Wang <100044659+ChrisWangCA@users.noreply.github.com>
2024-01-30 21:23:57 -0700

    Time: 1 ms (75.71%), Space: 41.1 MB (15.36%) - LeetHub

--- 0202-happy-number/0202-happy-number.java
+++ 0202-happy-number/0202-happy-number.java
@@ -3,16 +3,17 @@ class Solution {
         Set<Integer> set = new HashSet<>();
         while(n != 1 && !set.contains(n)){
             set.add(n);
-            n = getNext(n);
+            n = getN(n);
         }
         return n == 1;
     }
-    public int getNext(int n){
+    
+    public int getN(int n){
         int res = 0;
         while(n > 0){
             int temp = n % 10;
             res += temp * temp;
-            n = n/10;
+            n = n / 10;
         }
         return res;
     }
