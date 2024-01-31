d7fbe73b195badacc43095a0a268c9f60fd99ebc
Chris Wang <100044659+ChrisWangCA@users.noreply.github.com>
2024-01-30 21:52:47 -0700

    Time: 3 ms (63.95%), Space: 42.2 MB (65.99%) - LeetHub

--- 0093-restore-ip-addresses/0093-restore-ip-addresses.java
+++ 0093-restore-ip-addresses/0093-restore-ip-addresses.java
@@ -4,17 +4,14 @@ class Solution {
         StringBuilder sb = new StringBuilder(s);
         backtracing(sb,0,0);
         return res;
-        
     }
-    
-    public void backtracing(StringBuilder s, int startIndex, int dotSum){
+    public void backtracing(StringBuilder s, int startIndex,int dotSum){
         if(dotSum == 3){
             if(isValid(s,startIndex,s.length()-1)){
                 res.add(s.toString());
                 return;
             }
         }
-        
         for(int i=startIndex;i<s.length();i++){
             if(isValid(s,startIndex,i)){
                 s.insert(i+1,".");
@@ -25,15 +22,14 @@ class Solution {
             }
         }
     }
-    
     public boolean isValid(StringBuilder s,int start,int end){
-        int num = 0;
+        int res = 0;
         if(start > end) return false;
         if(s.charAt(start) == '0' && start != end) return false;
         for(int i=start;i<=end;i++){
-            int digit = s.charAt(i) -'0';
-            num = num * 10 + digit;
-            if(num > 255) return false;
+            int digit = s.charAt(i) - '0';
+            res = res * 10 + digit;
+            if(res > 255) return false;
         }
         return true;
     }
