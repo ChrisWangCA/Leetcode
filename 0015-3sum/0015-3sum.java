ab36ff1176512a62414011af8b8c71c9d21aba84
Chris Wang <100044659+ChrisWangCA@users.noreply.github.com>
2024-01-31 18:35:44 -0700

    Time: 26 ms (93.66%), Space: 51.2 MB (74.86%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,23 +1,23 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         List<List<Integer>> res = new ArrayList<>();
-        if(nums == null || nums.length < 2) return res;
         Arrays.sort(nums);
+        if(nums.length < 2 || nums == null) return res;
         for(int i=0;i<nums.length;i++){
-            if(nums[i] > 0) return res;
             if(i > 0 && nums[i] == nums[i-1]) continue;
-            int left = i+1;
+            if(nums[i] > 0) return res;
             int right = nums.length-1;
-            while(left < right){
-                int sum = nums[i] + nums[left] + nums[right];
+            int left = i+1;
+            while(right > left){
+                int sum = nums[i]+nums[left]+nums[right];
                 if(sum > 0){
                     right--;
                 }else if(sum < 0){
                     left++;
                 }else{
                     res.add(Arrays.asList(nums[i],nums[left],nums[right]));
-                    while(left<right && nums[left] == nums[left+1]) left++;
-                    while(left<right && nums[right] == nums[right-1]) right--;
+                    while(left<right && nums[left]==nums[left+1])left++;
+                    while(left<right && nums[right]==nums[right-1]) right--;
                     left++;
                     right--;
                 }
