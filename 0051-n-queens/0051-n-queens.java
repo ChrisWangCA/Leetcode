bf339d10bdf25a6a36488f4858af25e882a3c4c3
Chris Wang <100044659+ChrisWangCA@users.noreply.github.com>
2024-02-03 00:05:02 -0700

    Time: 2 ms (88.26%), Space: 44.9 MB (34.54%) - LeetHub

--- 0051-n-queens/0051-n-queens.java
+++ 0051-n-queens/0051-n-queens.java
@@ -5,10 +5,10 @@ class Solution {
         List<List<String>> res = new ArrayList<>();
         backtracing(res,record,0,n);
         return res;
-    }
+     }
     
-    public void backtracing(List<List<String>> res,int[] record,int row,int n){
-        if(row == n){
+    public void backtracing(List<List<String>> res,int[] record,int cur,int n){
+        if(cur == n){
             List<String> list = new ArrayList<>();
             for(int i=0;i<n;i++){
                 char[] chs = new char[n];
@@ -19,16 +19,16 @@ class Solution {
             res.add(list);
         }
         for(int i=0;i<n;i++){
-            if(isValid(record,row,i)){
-                record[row] = i;
-                backtracing(res,record,row+1,n);
+            if(isValid(record,cur,i)){
+                record[cur] = i;
+                backtracing(res,record,cur+1,n);
             }
         }
     }
     
-    public boolean isValid(int[] record, int row, int col){
+    public boolean isValid(int[] record,int row,int col){
         for(int i=0;i<row;i++){
-            if(record[i] == col || Math.abs(record[i]-col) == Math.abs(row - i)){
+            if(record[i] == col || Math.abs(record[i]-col) == Math.abs(row-i)){
                 return false;
             }
         }
