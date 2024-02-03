1f48989d82dc62667d25a5f62b3d213f199cb759
Chris Wang <100044659+ChrisWangCA@users.noreply.github.com>
2024-02-02 23:50:47 -0700

    Time: 8 ms (99.45%), Space: 45.4 MB (24.70%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.java
+++ 0455-assign-cookies/0455-assign-cookies.java
@@ -1,15 +1,16 @@
 class Solution {
     public int findContentChildren(int[] g, int[] s) {
-        int res = 0;
-        if(g.length == 0 || s.length == 0) return res;
+        if(g.length == 0 || s.length == 0) return 0;
         Arrays.sort(g);
         Arrays.sort(s);
+        int res = 0;
         int right = s.length-1;
         for(int i=g.length-1;i>=0;i--){
             if(right >= 0 && s[right] >= g[i]){
                 res++;
                 right--;
             }
+            
         }
         return res;
     }
