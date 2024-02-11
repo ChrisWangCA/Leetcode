2c3d126505c9683f635c234ca188e5388ee2be64
Chris Wang <100044659+ChrisWangCA@users.noreply.github.com>
2024-02-11 16:06:31 -0700

    Time: 0 ms (100.00%), Space: 41.7 MB (53.17%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.java
+++ 0101-symmetric-tree/0101-symmetric-tree.java
@@ -17,7 +17,6 @@ class Solution {
     public boolean isSymmetric(TreeNode root) {
         return isSame(root.left,root.right);
     }
-    
     public boolean isSame(TreeNode left,TreeNode right){
         if(left == null && right == null) return true;
         if(left == null || right == null || left.val != right.val) return false;
