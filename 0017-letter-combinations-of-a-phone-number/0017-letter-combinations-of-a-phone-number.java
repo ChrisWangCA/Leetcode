abffb4613170473d0c286e7f885b780c00d321c6
Chris Wang <100044659+ChrisWangCA@users.noreply.github.com>
2024-02-11 16:25:09 -0700

    Time: 0 ms (100.00%), Space: 41.6 MB (88.32%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -3,20 +3,19 @@ class Solution {
     StringBuilder sb = new StringBuilder();
     public List<String> letterCombinations(String digits) {
         if(digits.length() == 0 || digits == null) return res;
-        String[] str = {"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
-        backtracing(digits,str,0);
+        String[] strs = {"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+        backtracing(digits,strs,0);
         return res;
     }
-    
-    public void backtracing(String digits,String[] str,int num){
-        if(num == digits.length()){
+    public void backtracing(String digits,String[] strs,int nums){
+        if(nums == digits.length()){
             res.add(sb.toString());
             return;
         }
-        String s = str[digits.charAt(num)-'0'];
-        for(int i=0;i<s.length();i++){
-            sb.append(s.charAt(i));
-            backtracing(digits,str,num+1);
+        String str = strs[digits.charAt(nums)-'0'];
+        for(int i=0;i<str.length();i++){
+            sb.append(str.charAt(i));
+            backtracing(digits,strs,nums+1);
             sb.deleteCharAt(sb.length()-1);
         }
     }
