09242043344812f2093af441517a3c72e11f547c
Chris Wang <100044659+ChrisWangCA@users.noreply.github.com>
2024-02-11 19:43:50 -0700

    Time: 2 ms (88.05%), Space: 45 MB (27.70%) - LeetHub

--- 0051-n-queens/0051-n-queens.java
+++ 0051-n-queens/0051-n-queens.java
@@ -1,32 +1,32 @@
 class Solution {
-    List<List<String>> res = new ArrayList<>();
     public List<List<String>> solveNQueens(int n) {
+        List<List<String>> res = new ArrayList<>();
         int[] record = new int[n];
         Arrays.fill(record,0);
-        backtracing(record,0,n);
+        backtracing(res,record,0,n);
         return res;
     }
-    public void backtracing(int[] record,int cur,int n){
+    public void backtracing(List<List<String>> res,int[] record,int cur,int n){
         if(cur == n){
-            List<String> list = new ArrayList<>();
+            List<String> path = new ArrayList<>();
             for(int i=0;i<n;i++){
                 char[] chs = new char[n];
                 Arrays.fill(chs,'.');
                 chs[record[i]] = 'Q';
-                list.add(new String(chs));
+                path.add(new String(chs));
             }
-            res.add(list);
+            res.add(path);
         }
-        for(int i=0;i<n;i++){
-            if(isValid(record,cur,i)){
-                record[cur] = i;
-                backtracing(record,cur+1,n);
+        for(int j=0;j<n;j++){
+            if(isValid(record,cur,j)){
+                record[cur] = j;
+                backtracing(res,record,cur+1,n);
             }
         }
     }
-    public boolean isValid(int[] record,int cur, int j){
+    public boolean isValid(int[] record,int cur,int j){
         for(int i=0;i<cur;i++){
-            if(record[i] == j || Math.abs(record[i]-j) == Math.abs(cur-i)){
+            if(record[i] == j || Math.abs(record[i]-j) == Math.abs(cur - i)){
                 return false;
             }
         }
