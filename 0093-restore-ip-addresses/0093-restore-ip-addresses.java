896680e567c07392eb7c8fd5047b95fad404873c
Chris Wang <100044659+ChrisWangCA@users.noreply.github.com>
2024-02-14 00:07:26 -0700

    Time: 1 ms (99.46%), Space: 42.3 MB (69.54%) - LeetHub

--- 0093-restore-ip-addresses/0093-restore-ip-addresses.java
+++ 0093-restore-ip-addresses/0093-restore-ip-addresses.java
@@ -5,15 +5,14 @@ class Solution {
         backtracing(sb,0,0);
         return res;
     }
-    
-    public void backtracing(StringBuilder s, int startIndex, int sumDot){
+    public void backtracing(StringBuilder s,int startIndex,int sumDot){
         if(sumDot == 3){
             if(isValid(s,startIndex,s.length()-1)){
                 res.add(s.toString());
-                return;
             }
+            return;
         }
-        for(int i=startIndex;i<s.length();i++){
+        for(int i = startIndex;i<s.length();i++){
             if(isValid(s,startIndex,i)){
                 s.insert(i+1,".");
                 backtracing(s,i+2,sumDot+1);
@@ -22,12 +21,13 @@ class Solution {
                 break;
             }
         }
-        
     }
-    public boolean isValid(StringBuilder s, int start,int end){
-        int res = 0;
+    public boolean isValid(StringBuilder s,int start,int end){
         if(start > end) return false;
-        if(s.charAt(start) == '0' && start != end) return false;
+        int res = 0;
+        if(s.charAt(start) == '0' && start != end){
+            return false;
+        }
         for(int i=start;i<=end;i++){
             int n = s.charAt(i)-'0';
             res = res * 10 + n;
