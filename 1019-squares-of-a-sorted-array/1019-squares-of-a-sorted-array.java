059539be4d119184c250142f3984459ff4439709
Chris Wang <100044659+ChrisWangCA@users.noreply.github.com>
2024-08-15 19:09:13 -0600

    Time: 1 ms (100%), Space: 47.3 MB (25.36%) - LeetHub

--- 1019-squares-of-a-sorted-array/1019-squares-of-a-sorted-array.java
+++ 1019-squares-of-a-sorted-array/1019-squares-of-a-sorted-array.java
@@ -5,13 +5,11 @@ class Solution {
         int left = 0;
         int right = nums.length-1;
         while(left <= right){
-            int l = nums[left]*nums[left];
-            int r = nums[right] * nums[right];
-            if(l > r){
-                res[index--] = l;
+            if(nums[left] * nums[left] > nums[right] * nums[right]){
+                res[index--] = nums[left] * nums[left];
                 left++;
             }else{
-                res[index--] = r;
+                res[index--] = nums[right] * nums[right];
                 right--;
             }
         }
